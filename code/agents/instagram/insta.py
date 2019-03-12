16bb73ab278e169fc0a6a5b96351cd59b7de7373
Daniel Teixeira <danielteixeira@Daniels-MBP.home>
2019-03-12 22:44:31 +0000

    Elaboration PowerPoint

--- code/agents/instagram/insta.py
+++ code/agents/instagram/insta.py
@@ -9,4 +9,4 @@ loader = instaloader.Instaloader()
 loader.login("jorge.costa1@protonmail.com", "jorgecosta")
 
 # Search and Downloads public pictures with the hashtag="comidaportuguesa"
-loader.download_hashtag(, max_count=30,post_filter = lambda post: post.is_video == False, fast_update=True)
+loader.download_hashtag(hashtag="comidaportuguesa", max_count=30,post_filter = lambda post: post.is_video == False, fast_update=True)
