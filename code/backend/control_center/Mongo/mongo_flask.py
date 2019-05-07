b4848e4a7577172ea602f403777389d02202e5e0
joaoribeiro1 <joaoantonioribeiro1@hotmail.com>
2019-05-07 12:10:49 +0100

    primeira fase do user integrada. TODO: primeira fase do twitter

--- code/backend/control_center/Mongo/mongo_flask.py
+++ code/backend/control_center/Mongo/mongo_flask.py
@@ -6,11 +6,12 @@ class AppMongo:
 		appFlask.config["MONGO_URI"] = "mongodb://192.168.85.46:32769/"+db
 		self.app = PyMongo(appFlask)
 	
-	def dataCollection(self, findText={}):
+	def dataCollection(self, findText={},projection={}):
 		return list(self.app.db.collection.find(findText,projection={'_id':False}))
 
 	def getOneFilteredDoc(self, findText={},projection={'_id':False}):
 		return list(self.app.db.collection.find(findText,projection))
+
 
 	def aggregate(self,pipeline):
 		return list(self.app.db.collection.aggregate(pipeline))
