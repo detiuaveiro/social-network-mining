a8da6119467494b25b5b80a5efd6f6af7f0046c3
joaoribeiro1 <joaoantonioribeiro1@hotmail.com>
2019-05-11 23:18:16 +0100

    update PDP and PEP in this branch.still not complete. TODO:implement bot's in the db to complete it

--- code/backend/control_center/Messages_API/PEP.py
+++ code/backend/control_center/Messages_API/PEP.py
@@ -2,16 +2,18 @@ import json
 
 class PEP:
 
-    def __init__(self):
-        return
-
     def receive_message(self,msg):
         return self.formulate_request(msg)
         
-    '''
-    rever isto
-    '''
     def formulate_request(self,msg):
+        '''
+        This message comes from the messaging system. It could be one of the following:
+        - REQUEST_TWEET_LIKE = 5
+        - REQUEST_TWEET_RETWEET = 6
+        - REQUEST_TWEET_REPLY = 7
+        - REQUEST_FOLLOW_USER = 8
+        So, it is put in a dictionary, converted to json and sent to the PDP
+        '''
         intermediate={"name":msg}
         message=json.dumps(intermediate)
         return self.send_request(message)
