e92bc93d7041f86eafa99095ec407dac9f0c12bd
joaoribeiro1 <joaoantonioribeiro1@hotmail.com>
2019-05-12 23:08:19 +0100

    HOTFIX to the PDP, PEP and postgres driver

--- code/backend/control_center/api.py
+++ code/backend/control_center/api.py
@@ -21,17 +21,10 @@ neo=Neo4jAPI()
 
 @app.route("/")
 def home():
-<<<<<<< HEAD
     #a=postgres.addPolicy({"API_type":"Twitter"})
     p=neo.search_a_user()
     print(p)
     return "root"
-=======
-    #print(mongo.dataCollection(findText={"id":1103294806497902594}))
-    #mongo_t.removeData("")
-    #print(mongo_t.dataCollection())
-    return "root"#jsonify(mongo_t.dataCollection())
->>>>>>> Updated api.py
 
 @app.route("/twitter/users")
 def user_general():
@@ -43,19 +36,10 @@ def user_general():
         i["id"] = user_id
     return jsonify(mapa)
 
-<<<<<<< HEAD
 @app.route("/twitter/users/stats")
 def user_general_stats():
     stats=postgres.getAllStatsUsers()
     return jsonify(stats)
-=======
-##################################################################################    
-@app.route("/twitter/users/stats")
-def user_general_stats():
-    #stand by
-    return "user stats"
-##################################################################################
->>>>>>> Updated api.py
 
 @app.route("/twitter/users/<id>")
 def user_by_id(id):
@@ -66,15 +50,9 @@ def user_by_id(id):
             mapa[0]["id"]=(str(id))
             return jsonify(mapa)
         else:
-<<<<<<< HEAD
-            return jsonify({"Error":"wrong id"})
-    except TypeError:
-        return jsonify({"Error":"invalid"})
-=======
             return jsonify({"error":"wrong id"})
     except TypeError:
         return jsonify({"error":"invalid"})
->>>>>>> Updated api.py
 
 @app.route("/twitter/users/<id>/tweets")
 def user_tweets(id):
@@ -83,16 +61,9 @@ def user_tweets(id):
 
 @app.route("/twitter/users/<id>/followers")
 def user_followers(id):
-<<<<<<< HEAD
     followers = mongo.getOneFilteredDoc(findText={"id":int(id)},projection={"followers_count":True,"_id":False})
     return jsonify(followers)
 
-=======
-    
-    followers = mongo.getOneFilteredDoc(findText={"id":int(id)},projection={"followers_count":True,"_id":False})
-    return jsonify(followers)
-    
->>>>>>> Updated api.py
 @app.route("/twitter/users/<id>/following")
 def user_following(id):
     mapa=mongo.getOneFilteredDoc(findText={"id":int(id)},projection={"friends_count":True,"_id":False})
@@ -117,11 +88,7 @@ def user_replies(id):
 
 @app.route("/twitter/users/<id>/stats")
 def user_stats(id):
-<<<<<<< HEAD
     stats=postgres.getStatsUserID(id) #mongo.getOneFilteredDoc(findText={"id":int(id)},projection={"favourites_count":True,"followers_count":True,"friends_count":True,"location":True,"name":True,"screen_name":True,"statuses_count":True,"verified":True,"_id":False})
-=======
-    stats=mongo.getOneFilteredDoc(findText={"id":int(id)},projection={"favourites_count":True,"follow_request_sent":True,"followers_count":True,"following":True,"friends_count":True,"lang":True,"location":True,"name":True,"screen_name":True,"statuses_count":True,"verified":True,"_id":False})
->>>>>>> Updated api.py
     return jsonify(stats)
 
 '''
@@ -133,7 +100,6 @@ def tt_network():
 
 @app.route("/twitter/policies")
 def tt_policies():
-<<<<<<< HEAD
     mapa=policy.getPoliciesByAPI("Twitter")
     return jsonify(mapa)
 
@@ -147,19 +113,6 @@ def tt_bots():
     val=neo.search_bot()
     print(val)
     return val #json.dumps(json_to_send["users"]["1103294806497902594"])
-=======
-    return "twitter"
-
-##################################################################################
-@app.route("/twitter/stats")
-def tt_stats():
-    return "twitter"
-##################################################################################
-
-@app.route("/twitter/bots")
-def tt_bots():
-    return "TBD" #json.dumps(json_to_send["users"]["1103294806497902594"])
->>>>>>> Updated api.py
 
 @app.route("/twitter/bots/<id>")
 def tt_bots_by_id(id):
@@ -176,7 +129,6 @@ def tt_tweets():
     mapa=mongo_t.dataCollection()
     return jsonify(mapa)
 
-<<<<<<< HEAD
 @app.route("/twitter/tweets/stats")
 def tt_tweet_stats():
     #agregação de likes, retweets
@@ -192,31 +144,6 @@ def tt_tweet_stats():
         ret+=i["retweet"]'''
     stats=postgres.getAllStatsTweets()
     return jsonify(stats)#jsonify({"favorite_count":fav,"retweet_count":ret})
-=======
-##################################################################################
-@app.route("/twitter/tweets/stats")
-def tt_tweet_stats():
-    #agregação de likes, retweets
-    '''
-    from bson.son import SON
-    pipeline = [
-        {"$unwind": "$tags"},
-        {"$group": {"_id": "$tags", "count": {"$sum": 1}}},
-        {"$sort": SON([("count", -1), ("_id", -1)])}
-    ]
-    import pprint
-    pprint.pprint(list(db.things.aggregate(pipeline)))
-    '''
-    pipeline=[
-        {"$unwind": "$favorite_count"},
-        #{"$unwind": "$retweet_count"},
-        {"$group": {"_id": "$favorite_count", "count": {"$sum": 1}}},
-        #{"$group": {"_id": "$retweet_count", "retweet_count": {"$sum": 1}}},
-    ]
-    mapa=mongo_t.aggregate(pipeline)
-    return jsonify(mapa)
-##################################################################################
->>>>>>> Updated api.py
 
 @app.route("/twitter/tweets/<id>")
 def tt_tweet_by_id(id):
@@ -224,30 +151,19 @@ def tt_tweet_by_id(id):
         mapa=mongo_t.dataCollection(findText={"id":int(id)})
         return jsonify(mapa)
     except TypeError:
-<<<<<<< HEAD
-        return jsonify({"Error":"invalid"})
-=======
         return jsonify({"error":"invalid"})
->>>>>>> Updated api.py
     
     
 @app.route("/twitter/tweets/<id>/stats")
 def tt_tweet_stats_by_id(id):
-<<<<<<< HEAD
     #mapa=mongo_t.getOneFilteredDoc(findText={"id":int(id)},projection={"created_at":True,"entities.hashtags":True,"entities.user_mentions.name":True,"entities.user_mentions.screen_name":True,"favorited":True,"in_reply_to_screen_name":True,"in_reply_to_status_id_str":True,"in_reply_to_user_id_str":True,"is_quote_status":True,"place":True,"favorite_count":True,"retweet_count":True,"retweeted":True,'user.id_str':True,'user.name':True,'user.screen_name':True,'_id':False})
     stats=postgres.getStatsTweetID(id)
     return jsonify(stats)
-=======
-    mapa=mongo_t.getOneFilteredDoc(findText={"id":int(id)},projection={"created_at":True,"entities.hashtags":True,"entities.user_mentions.name":True,"entities.user_mentions.screen_name":True,"favorited":True,"geo":True,"in_reply_to_screen_name":True,"in_reply_to_status_id":True,"in_reply_to_status_id_str":True,"in_reply_to_user_id":True,"in_reply_to_user_id_str":True,"is_quote_status":True,"place":True,"favorite_count":True,"retweet_count":True,"retweeted":True,'user.id_str':True,'user.name':True,'user.screen_name':True,'_id':False})
-    return jsonify(mapa)
-
->>>>>>> Updated api.py
 '''
 policies paths
 '''
 @app.route("/policies")
 def policies():
-<<<<<<< HEAD
     mapa=policy.getAllPolicies()
     return jsonify(mapa)
 
@@ -312,29 +228,13 @@ def update_policy():
         return jsonify(send)
 ##################################################################################################################################
 
-=======
-    return "<h1>policies</h1>"
-
-@app.route("/policies/<id>")
-def policies_by_id(id):
-    return "<h1>policies by id</h1>"
-
-@app.route("/policies/bots/<id>")
-def policies_by_bot(id):
-    return "<h1>policies by bot</h1>"
-##########################################################################################################################
->>>>>>> Updated api.py
 '''
 instagram paths
 '''
 @app.route("/instagram/policies")
 def ig_policies():
-<<<<<<< HEAD
     mapa=policy.getPoliciesByAPI("Instagram")
     return jsonify(mapa)
-=======
-    return "ig policies"
->>>>>>> Updated api.py
 
 @app.route("/instagram/stats")
 def ig_stats():
@@ -352,13 +252,5 @@ def bot_by_id(id):
 def bot_logs(id):
     return "bot logs"
 
-<<<<<<< HEAD
-=======
-@app.route("/timescale")
-def timescale():
-    return "<h1>timescale</h1>"
-
-
->>>>>>> Updated api.py
 if __name__ == "__main__":
     app.run(debug=True)
