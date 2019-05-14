54b3b8ab3df58622f21b6c4f7d9aec15784444b7
Miguel F <miguel.fradinho@ua.pt>
2019-05-14 19:13:07 +0100

    [refactor-bots] Bugfixing MessageType
    - Changed the value to not clash with another enum constant.
    - Minor documentation to both enums classes

--- code/agents/twitter/enums.py
+++ code/agents/twitter/enums.py
@@ -2,6 +2,9 @@ from enum import IntEnum
 
 
 class Task(IntEnum):
+    """
+    Enum for the tasks the bot is able to run. Received by the bot from the server.
+    """
     FOLLOW_USERS = 1
     FIND_BY_KEYWORDS = 2
     LIKE_TWEETS = 3
@@ -12,17 +15,20 @@ class Task(IntEnum):
 
 
 class MessageType(IntEnum):
+    """
+    Enum for the Messages sent by the bot to the server.
+    """
     EVENT_USER_FOLLOWED = 1
     EVENT_TWEET_LIKED = 2
     EVENT_TWEET_RETWEETED = 3
     EVENT_TWEET_REPLIED = 4
-    EVENT_USER_BLOCKED = 5
     QUERY_TWEET_LIKE = 5
     QUERY_TWEET_RETWEET = 6
     QUERY_TWEET_REPLY = 7
     QUERY_FOLLOW_USER = 8
     SAVE_USER = 9
     SAVE_TWEET = 10
+    EVENT_USER_BLOCKED = 11 
 
     def __str__(self):
         return self.name
