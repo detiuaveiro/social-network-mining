30311c32e1edd5e3d1423606e95b5c47256b7c4e
Daniel Teixeira <danielteixeira@aluno-6848.wireless.ua.pt>
2019-05-15 15:18:39 +0100

    FIX MERGE

--- code/backend/control_center/Messages_API/message_center.py
+++
@@ -1,6 +0,0 @@
-from rabbitmq import Rabbitmq
-
-if __name__ == "__main__":
-    rabbit = Rabbitmq(host='mqtt-redesfis.5g.cn.atnog.av.it.pt', port=5672, vhost="PI",username='pi_rabbit_admin', password='yPvawEVxks7MLg3lfr3g')
-    rabbit.receive(q='API')
-    rabbit.close()
