efc6bbe2ddac76eae0163307e7fea423a529b830
José Gonçalves <apenaspara1998@gmail.com>
2019-05-17 12:05:49 +0100

    functionality of adding, editing and removing policies fully implemented without error handling.

--- code/dashboard/src/components/DataTables/DataTables.jsx
+++ code/dashboard/src/components/DataTables/DataTables.jsx
@@ -4,7 +4,14 @@ import React from "react";
 // core components
 import { Button, Checkbox } from "components";
 import {
-    Table
+    Row,
+    Col,
+    Table,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Modal, ModalHeader, ModalBody, ModalFooter,
 } from "reactstrap";
 
 import axios from 'axios';
@@ -13,14 +20,13 @@ class ReactTables extends React.Component {
     /*
      */
     state = {
-        policies: this.props.policies,
-        editPolicy: this.props.editPolicy || function() {}
+        policies: []
     };
     /**
      * [
      *   {
      *     name : "Example name",
-     *     api_type : "Twitter",
+     *     API_type : "Twitter",
      *     filter : "keywords",
      *     params : ["keyword1","keyword2","keyword3"],
      *     id_policy : "something"
@@ -31,21 +37,34 @@ class ReactTables extends React.Component {
     constructor(props) {
         super(props);
 
+        this.modalRef = React.createRef();
+
         this.clickRemovePolicy = this.clickRemovePolicy.bind(this);
         this.clickEditPolicy = this.clickEditPolicy.bind(this);
+        this.loadPolicies = this.loadPolicies.bind(this);
     }
 
     componentDidMount() {
-        this.updatePolicies();
+        this.loadPolicies();
     }
 
-    updatePolicies() {
-        axios.post('/policies')
-            .then(res => {
-                const policies = res.policies;
+    loadPolicies() {
+        axios.get('/policies')
+             .then(res => {
+                const policies = res.data;
                 this.setState({ policies });
                 console.log(policies);
             });
+    }
+
+    updatePolicy(policy) {
+        axios.post("/policies/update", { policy })
+            .then(
+                res => {
+                    console.log(res);
+                    this.loadPolicies();
+                }
+            );
     }
 
     clickRemovePolicy(e) {
@@ -53,19 +72,24 @@ class ReactTables extends React.Component {
         console.log(id);
         axios.get('/policies/remove/' + id)
             .then(res => {
-                this.updatePolicies();
+                console.log(res);
+                this.loadPolicies();
             });
     }
 
     clickEditPolicy(e) {
         let index = parseInt(e.currentTarget.parentElement.parentElement.dataset.index);
-        this.state.editPolicy(this.state.policies[0]);
+        let policy = this.state.policies[index];
+        console.log(policy);
+
+        this.modalRef.current.setPolicy(policy);
+        this.modalRef.current.toggle();
     }
 
     render() {
         return (
             <div>
-                <Table responsive striped size>
+                <Table responsive striped>
                     <thead className="text-primary">
                         <tr>
                             <th className="text-center">#</th>
@@ -80,13 +104,13 @@ class ReactTables extends React.Component {
                     </thead>
                     <tbody>
                         {this.state.policies.map(function (policy, index) {
-                            return (<tr id={policy.id_policy} data-index={index}>
-                                <td className="text-center">{index+1}</td>
+                            return (<tr id={policy.id_policy} key={index.toString()} data-index={index}>
+                                <td className="text-center">{index + 1}</td>
                                 <td className="text-center">
                                     <Checkbox />
                                 </td>
                                 <td>{policy.name}</td>
-                                <td>{policy.api_type}</td>
+                                <td>{policy.API_type}</td>
                                 <td className="text-center">{policy.filter}</td>
                                 <td className="text-right">{policy.params.join()}</td>
                                 <td className="text-right">{policy.target}</td>
@@ -102,8 +126,99 @@ class ReactTables extends React.Component {
                         }.bind(this))}
                     </tbody>
                 </Table>
+                <EditPoliciesModal ref={this.modalRef} modal={this.state.edit_mode} submitCallBack={this.updatePolicy} ></EditPoliciesModal>
             </div>
         );
     }
 }
+
+class EditPoliciesModal extends React.Component {
+
+    state = {
+        modal: this.props.modal !== undefined ? this.props.modal : false,
+        policy: this.props.policy !== undefined ? this.props.policy : {
+            params: []
+        }
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.submitUpdatePolicy = this.submitUpdatePolicy.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+    }
+
+    setPolicy(policy) {
+        this.setState({
+            policy: policy
+        });
+    }
+
+    submitUpdatePolicy() {
+        let API_type = document.getElementById("api_type").value;
+        let name = document.getElementById("name").value;
+        let filter = document.getElementById("filter").value;
+        let params = document.getElementById("params").value.split(",");
+
+        let updateMessage = {
+            id_policy: this.state.policy.id_policy,
+            API_type: API_type,
+            name: name,
+            filter: filter,
+            params: params
+        };
+
+        this.toggle();
+        if (this.props.submitCallBack) { this.props.submitCallBack(updateMessage); }
+    }
+
+    render() {
+        return (
+            <div>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader>Edit Policy</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup row>
+                                <Label for="api_type" sm={2}>Social Network</Label>
+                                <Col sm={10}>
+                                    <Input type="text" name="API_type" id="api_type" placeholder="Social Network to apply this filter" defaultValue={this.state.policy.API_type} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="name" sm={2}>Name</Label>
+                                <Col sm={10}>
+                                    <Input type="text" name="name" id="name" placeholder="Name of the policy" defaultValue={this.state.policy.name} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="filter" sm={2}>Filter Type</Label>
+                                <Col sm={10}>
+                                    <Input type="text" name="filter" id="filter" placeholder="Filter type" defaultValue={this.state.policy.filter} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="params" sm={2}>Parameters</Label>
+                                <Col sm={10}>
+                                    <Input type="text" name="params" id="params" placeholder="Parameters" defaultValue={this.state.policy.params.join()} />
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.submitUpdatePolicy}>Update</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
+
 export default ReactTables;
--- code/dashboard/src/views/Policies/Policies.jsx
+++ code/dashboard/src/views/Policies/Policies.jsx
@@ -3,23 +3,46 @@ import {
   Card,
   CardHeader,
   CardBody,
+  CardFooter,
   CardTitle,
   Row,
-  Col
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Modal, ModalHeader, ModalBody, ModalFooter,
+  FormText
 } from "reactstrap";
 
 import { PanelHeader, DataTables } from "components";
+import axios from "axios";
 
 class Policies extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.editPolicy = this.editPolicy.bind(this);
+    this.dataTablesRef = React.createRef();
+    this.modalRef = React.createRef();
+
+    this.clickAddPolicy = this.clickAddPolicy.bind(this);
+    this.submitAddPolicy = this.submitAddPolicy.bind(this);
   }
 
-  editPolicy(policy) {
-    
+  clickAddPolicy(e) {
+    this.modalRef.current.toggle();
+  }
+
+  submitAddPolicy(policy) {
+    console.log(policy);
+    axios.post("/policies/add", { policy })
+      .then(res => {
+        console.log(res);
+        this.modalRef.toggle();
+        this.dataTablesRef.current.loadPolicies();
+      });
   }
 
   render() {
@@ -42,21 +65,111 @@ class Policies extends React.Component {
                 </CardHeader>
                 <CardBody>
                   <DataTables policies={[{
-                    name : "Example name",
-                    api_type : "Twitter",
-                    filter : "keywords",
-                    params : ["keyword1","keyword2","keyword3"],
-                    id_policy : "something",
-                    target : "..."
+                    name: "Example name",
+                    API_type: "Twitter",
+                    filter: "keywords",
+                    params: ["keyword1", "keyword2", "keyword3"],
+                    id_policy: "something",
+                    target: "..."
                   }]}
-                   editPolicy={this.editPolicy} />
+                    ref={this.dataTablesRef} />
 
                 </CardBody>
+                <CardFooter>
+                  <Row>
+                    <Col sm={{ size: 1, offset: 11 }}>
+                      <Button color="success" onClick={this.clickAddPolicy}>
+                        <i className="now-ui-icons ui-1_simple-add"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </CardFooter>
               </Card>
             </Col>
           </Row>
         </div>
+
+        {/*The modals stay down here */}
+        <AddPoliciesModal ref={this.modalRef} submitCallBack={this.submitAddPolicy}></AddPoliciesModal>
       </div>
+    );
+  }
+}
+
+class AddPoliciesModal extends React.Component {
+
+  state = {
+    modal: false,
+    submitCallBack: this.props.submitCallBack !== undefined ? this.props.submitCallBack : function () { }
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.submitPolicy = this.submitPolicy.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  submitPolicy(e) {
+    let API_type = document.getElementById("api_type").value;
+    let name = document.getElementById("name").value;
+    let filter = document.getElementById("filter").value;
+    let params = document.getElementById("params").value.split(",");
+
+    let newPolicy = {
+      API_type: API_type,
+      name: name,
+      filter: filter,
+      params: params
+    };
+
+    this.toggle();
+    this.props.submitCallBack(newPolicy);
+  }
+
+  render() {
+    return (
+      <Modal isOpen={this.state.modal} toggle={this.toggle}>
+        <ModalHeader>Add Policy</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup row>
+              <Label for="api_type" sm={2}>Social Network</Label>
+              <Col sm={10}>
+                <Input type="text" name="API_type" id="api_type" placeholder="Social Network to apply this filter" />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="name" sm={2}>Name</Label>
+              <Col sm={10}>
+                <Input type="text" name="name" id="name" placeholder="Name of the policy" />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="filter" sm={2}>Filter Type</Label>
+              <Col sm={10}>
+                <Input type="text" name="filter" id="filter" placeholder="Filter type" />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="params" sm={2}>Parameters</Label>
+              <Col sm={10}>
+                <Input type="text" name="params" id="params" placeholder="Parameters" />
+              </Col>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.submitPolicy}>Add</Button>{' '}
+          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
     );
   }
 }
