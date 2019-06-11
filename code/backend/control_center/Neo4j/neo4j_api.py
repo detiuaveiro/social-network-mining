df6531bc5aa365d56ff06ae089512a8bcb651b34
joaoribeiro1 <joaoantonioribeiro1@hotmail.com>
2019-06-11 01:13:09 +0100

    #3 network probably fixed

--- code/backend/control_center/Neo4j/neo4j_api.py
+++ code/backend/control_center/Neo4j/neo4j_api.py
@@ -181,7 +181,7 @@ class Neo4jAPI():
     def export_network(self):
         with self._driver.session() as session:
             result=session.run("call apoc.export.csv.query('match (b)-[r:FOLLOWS]->(u) return b.id,b.name,b.username,type(r),u.id,u.name,u.username;','file.csv',{})")
-        #os.system("scp alunos@192.168.85.187:/var/lib/neo4j/import/file.csv /")
+        os.system("scp alunos@192.168.85.187:/var/lib/neo4j/import/file.csv /")
         data=open("/file.csv","r+")
         return data
             
