2e41cd627f2f123bd46087b6ff363bd9fe6a7595
escaleira <pedroescaleira@hotmail.com>
2020-03-02 03:46:54 +0000

    done

--- code/backend/twitter/rest/api/views/users.py
+++ code/backend/twitter/rest/api/views/users.py
@@ -5,8 +5,21 @@ from api import queries
 from api.views.utils import create_response
 
 
+@api_view(["GET"])
 def twitter_users(request):
-	return None
+	error_messages = []
+	success_messages = []
+	status = HTTP_200_OK
+
+	success, data, message = queries.twitter_users()
+	if success:
+		success_messages.append(message)
+	else:
+		error_messages.append(message)
+		status = HTTP_403_FORBIDDEN
+
+	return create_response(data=data, error_messages=error_messages,
+						   success_messages=success_messages, status=status)
 
 
 def twitter_users_stats(request):
