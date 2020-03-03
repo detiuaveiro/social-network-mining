204d07170acf94e741bcf03330fd8b5cafdf20b5
escaleira <pedroescaleira@hotmail.com>
2020-03-03 23:59:43 +0000

    postgres tweets table creation and tweets stats serializers

--- code/backend/twitter/examples/postgres/tables.sql
+++ code/backend/twitter/examples/postgres/tables.sql
@@ -1,12 +1,13 @@
 // User DB
 
-create table tweets(
-    timestamp,
-    tweet_id,
-    user_id,
-    likes,
-    retweets
-)
+CREATE TABLE "tweets"(
+    "tweet_id" integer NOT NULL PRIMARY KEY, 
+    "user_id" integer NOT NULL, 
+    "likes" integer NOT NULL, 
+    "retweets" integer NOT NULL, 
+    "timestamp" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 
 create table user(
     timestamp,
@@ -25,12 +26,10 @@ create table logs(
 
 create table policies(
     id integer,
-    API_type choice("Twitter","Instagram"),
-    filter choice("Keywords","Username"),
+    API_type choice("Twitter", "Instagram"),
+    filter choice("Keywords", "Username"),
     name string,
     tags String[],
     bots Integer[],
     active boolean
 )
-
-
