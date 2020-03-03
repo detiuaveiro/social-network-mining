ea23fb5a48bdad281cfa5a7c352c876cf9ada7f0
escaleira <pedroescaleira@hotmail.com>
2020-03-03 14:03:20 +0000

    done

--- code/backend/twitter/rest/api/queries.py
+++ code/backend/twitter/rest/api/queries.py
@@ -29,10 +29,22 @@ def twitter_user(id):
 
 def twitter_users_stats():
 	try:
-		return True, [serializers.UserStats(us).data for us in UserStats.objects.all()], "Sucesso a obter as estatisticas do utilizador pedido"
+		return True, [serializers.UserStats(us).data for us in UserStats.objects.all()], \
+			   "Sucesso a obter as estatisticas do utilizador pedido"
 	except Exception as e:
 		logger.error(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {e}")
 		return False, None, f"Erro as estatisticas de todos os utilizadores"
+
+
+def twitter_user_stats(id):
+	try:
+		return True, serializers.UserStats(UserStats.objects.get(user_id=id)).data, \
+			   "Sucesso a obter as estatisticas do utilizador pedido"
+	except UserStats.DoesNotExist:
+		return False, None, f"Não existe nenhum utilizador com o id {id} na base de dados de estatisticas"
+	except Exception as e:
+		logger.error(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {e}")
+		return False, None, f"Erro as estatisticas do utilizador de id {id}"
 
 
 ################# tweets #################
--- code/backend/twitter/rest/api/views/users.py
+++ code/backend/twitter/rest/api/views/users.py
@@ -63,8 +63,21 @@ def twitter_user_following(request, id):
 	return None
 
 
+@api_view(["GET"])
 def twitter_user_stats(request, id):
-	return None
+	error_messages = []
+	success_messages = []
+	status = HTTP_200_OK
+
+	success, data, message = queries.twitter_user_stats(int(id))
+	if success:
+		success_messages.append(message)
+	else:
+		error_messages.append(message)
+		status = HTTP_403_FORBIDDEN
+
+	return create_response(data=data, error_messages=error_messages,
+						   success_messages=success_messages, status=status)
 
 
 def twitter_users_export(request):
