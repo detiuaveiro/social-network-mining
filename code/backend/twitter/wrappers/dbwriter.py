10b1893046500d9124ad93efa121ab1f12cf2c40
DrPunpun <pedrooliveira99@ua.pt>
2020-03-03 10:59:07 +0000

    documentation fixed, but need some info to finish the functions

--- code/backend/twitter/wrappers/dbwriter.py
+++ code/backend/twitter/wrappers/dbwriter.py
@@ -1,10 +1,10 @@
-import mongo
-import neo4j
-import postgres
-import rabbitmq
 #import policy_api
+# import policy_api
 import logging
 import sys
+
+from mongo_wrapper import MongoAPI
+
 sys.path.append("..")
 from enums import *
 
@@ -26,6 +26,7 @@ class DBWriter:
         """
         #Falta as API
         #...
+        self.mongo_client = MongoAPI()
         pass
 
     def action(self, message):
@@ -72,155 +73,137 @@ class DBWriter:
 
 
     #Need DB API now
-    def follow_user(self, message):
+    def follow_user(self, data):
         """
         Action to follow user:
             Calls neo4j to add new relation between bot and user
             Calls postgres_stats to add new log with the action details
 
-        params:
-        ------
-        message: dict containing bot and the user he's following
+        @param data: dict containing bot and the user he's following
         """
         pass
 
-    def like_tweet(self, message):
+    def like_tweet(self, data):
         """
         Action to like tweet:
             Calls postgres_stats to add new log
         
-        params:
-        -------
-        message: dict with the bot id and the tweet he liked
+        @param data dict with the bot id and the tweet he liked
         """
         pass
 
-    def retweet(self, message):
+    def retweet(self, data):
         """
         Action to retweet:
             Calls postgres_stats to add new log
 
-        params:
-        ------
-        message: dict containing bot and the tweet he retweeted
+        @param data: dict containing bot and the tweet he retweeted
         """
         pass
 
-    def reply_tweet(self, message):
+    def reply_tweet(self, data):
         """
         Action to reply a tweet:
             Calls progres_stats to add what the bot replied and to which tweet
 
-        params:
-        ------
-        message: dict contaning bot and the reply they made
+        @param data: dict contaning bot and the reply they made
         """
         pass
 
-    def request_tweet_like(self, message):
+    def request_tweet_like(self, data):
         """
         Action to request a like on tweeter:
             Calls the control center to request the like
             Adds the log to postgres_stats, for the request and its result
             The result is based on the Policy API object
         
-        params:
-        ------
-        message: dict containing the bot id and the tweet id
+        @param data: dict containing the bot id and the tweet id
         """
         pass
 
-    def request_retweet(self, message):
+    def request_retweet(self, data):
         """
         Action to request a retweet:
             Calls the control center to request the retweet
             Adds the log to postgres_stats, for the request and its result
             The result is based on the Policy API object
         
-        params:
-        ------
-        message: dict containing the bot id and the tweet id
+        2param data: dict containing the bot id and the tweet id
         """
         pass
 
-    def request_tweet_reply(self, message):
+    def request_tweet_reply(self, data):
         """
         Action to request a reply:
             Calls the control center to request the reply
             Adds the log to postgres_stats, for the request and its result
             The result is based on the Policy API object
         
-        params:
-        ------
-        message: dict containing the bot id and the tweet id
+        @param data: dict containing the bot id and the tweet id
         """
         pass
 
-    def request_follow_user(self, message):
+    def request_follow_user(self, data):
         """
         Action to request a follow:
             Calls the control center to request the follow
             Adds the log to postgres_stats, for the request and its result
             The result is based on the Policy API object
         
-        params:
-        ------
-        message: dict containing the bot id and the tweet id
+        @param data: dict containing the bot id and the tweet id
         """
         pass
 
-    def save_user(self, message):
+    def save_user(self, data):
         """
         Stores info about a user:
-            Calls the neo4j and the mongo object to update or store the user be it a bot r a user)
+            Calls the neo4j and the mongo object to update or store the user be it a bot or a user)
             Adds the log of the operation to postgress_stats
             If the user is a bot, must also call the Policy API object
-        params:
-        ------
-        message: dict containing the id of the bot and the user object
+        
+        @param data: dict containing the id of the bot and the user object
         """
+
+        log.info("Saving User")
         pass
 
-    def save_tweet(self, message):
+    def save_tweet(self, data):
         """
         Stores info about a tweet:
             Calls the mongo object to save or update a tweet
             Adds the operation log to postgress_stats
-        params
-        ------
-        message: dict containing the id of the tweet to bee saved
+        
+        @param data: dict containing the id of the tweet to bee saved
         """
         pass
 
-    def save_dm(self, message):
+    def save_dm(self, data):
         """
         Stores the info about a direct message:
             Calls the mongo object to save or update a dm
             Adds the operation log to postgress_stats
-        params
-        ------
-        message: dict containignt the id of the bot and the dm
+        
+        @param data: dict containignt the id of the bot and the dm
         """
         pass
 
-    def error(self, message):
+    def error(self, data):
         """
         Stores error that may have occured in the running of a bot:
             Calls the postgres stats to log the error
-        params
-        ------
-        message: dict with the id of a bot and the error object
+        
+        @param data: dict with the id of a bot and the error object
         """
         pass
 
-    def find_followers(self, message):
+    def find_followers(self, data):
         """
         Saves the followers for a given user in the graph database:
             May need to create the user and its followers from scratch or just update their respective info
         
         params
         ------
-        message: dict with the id of a user and a list of user IDs
+        data: dict with the id of a user and a list of user IDs
         """
         pass
 
@@ -228,11 +211,9 @@ class DBWriter:
         """
         Function the task uses to send messages through rabbit
 
-        params
-        ------
-        bot: id of the bot to reply
-        message_type: ResponseTypes object with the type of message
-        params: dict with arguments of the message
+        @param bot: id of the bot to reply
+        @param message_type: ResponseTypes object with the type of message
+        @param params: dict with arguments of the message
         """
         log.info(f"Sending {message_type.name} to Bot with ID: <{bot}>")
         log.debug(f"Content: {params}")
