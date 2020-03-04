db2b8bb4f02da74d27089970c5e4a84ddb3cf180
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-04 23:53:01 +0000

    init

--- code/backend/twitter/rest/api/views/bots.py
+++ code/backend/twitter/rest/api/views/bots.py
@@ -1,3 +1,9 @@
+from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_200_OK, HTTP_403_FORBIDDEN
+
+from api import queries
+from api.views.utils import create_response
+
 def twitter_create(request):
 	return None
 
