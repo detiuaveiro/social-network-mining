e4ff578cfffda5f4c9ac08b1289c595a36379eb4
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-05 14:03:44 +0000

    feature/rest/bots/twitter_bot_messages

--- code/backend/twitter/rest/api/views/bots.py
+++ code/backend/twitter/rest/api/views/bots.py
@@ -1,3 +1,9 @@
+from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_200_OK, HTTP_403_FORBIDDEN
+
+from api import queries
+from api.views.utils import create_response
+
 def twitter_create(request):
 	return None
 
@@ -17,6 +23,23 @@ def twitter_bot(request, id):
 def twitter_bot_logs(request, id):
 	return None
 
+@api_view(["GET"])
+def twitter_bot_messages(_, id):
+	"""
+	Return all privates messages from a bot
+	:param id: bot's id
+	:return: response object
+	"""
+	error_messages = []
+	success_messages = []
+	status = HTTP_200_OK
 
-def twitter_bot_messages(request, id):
-	return None
+	success, data, message = queries.twitter_bot_messages(id)
+	if success:
+		success_messages.append(message)
+	else:
+		error_messages.append(message)
+		status = HTTP_403_FORBIDDEN
+
+	return create_response(data=data, error_messages=error_messages,
+						   success_messages=success_messages, status=status)
