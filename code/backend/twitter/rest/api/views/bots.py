5297ce9bcba2803d6352c6a4e149443181255281
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-06 02:23:47 +0000

    neo4j bots ids script converter added

--- code/backend/twitter/rest/api/views/bots.py
+++ code/backend/twitter/rest/api/views/bots.py
@@ -39,26 +39,27 @@ def twitter_stats(_):
 def twitter_bots(request):
     return None
 
+
 @api_view(["GET"])
 def twitter_bot(_, id):
-	"""
-	Returns a info associated to a bot saved on neo4j
-	:param id: bot's id
-	:return: response Object
-	"""
-	error_messages = []
-	success_messages = []
-	status = HTTP_200_OK
+    """
+    Returns a info associated to a bot saved on neo4j
+    :param id: bot's id
+    :return: response Object
+    """
+    error_messages = []
+    success_messages = []
+    status = HTTP_200_OK
 
-	success, data, message = queries.twitter_bot(int(id))
-	if success:
-		success_messages.append(message)
-	else:
-		error_messages.append(message)
-		status = HTTP_403_FORBIDDEN
+    success, data, message = queries.twitter_bot(int(id))
+    if success:
+        success_messages.append(message)
+    else:
+        error_messages.append(message)
+        status = HTTP_403_FORBIDDEN
 
-	return create_response(data=data, error_messages=error_messages,
-						   success_messages=success_messages, status=status)
+    return create_response(data=data, error_messages=error_messages,
+                           success_messages=success_messages, status=status)
 
 
 def twitter_bot_logs(request, id):
