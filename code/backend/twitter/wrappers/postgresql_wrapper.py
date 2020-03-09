f16b0e359e404e0060e51f70ab52e64912f5e555
HerouFenix <diogo04@ua.pt>
2020-03-09 20:52:05 +0000

    lets go bois

--- code/backend/twitter/wrappers/postgresql_wrapper.py
+++ code/backend/twitter/wrappers/postgresql_wrapper.py
@@ -15,18 +15,17 @@ handler.setFormatter(
 log.addHandler(handler)
 
 
-class PostgresAnalysisAPI:
+class PostgresAPI:
     """PostgreSQL
 
     Encompasses methods used for all API and PDP interactions with our PostgreSQL DB.
-    Interacts with the snm_analysis_pg (for time series analysis) database
     """
 
     def __init__(self):
         log.debug("Connecting to PostgreSQL Analysis")
         try:
             # Connect to the PostgreSQL server
-            self.conn = psycopg2.connect(host=credentials.POSTGRES_URL, database=credentials.POSTGRES_ANALYSIS_DB,
+            self.conn = psycopg2.connect(host=credentials.POSTGRES_URL, database=credentials.POSTGRES_DB,
                                          user=credentials.POSTGRES_USERNAME, password=credentials.POSTGRES_PASSWORD)
 
         except (Exception, psycopg2.DatabaseError) as error:
@@ -185,89 +184,20 @@ class PostgresAnalysisAPI:
             return {"success": False, "error": error}
 
 
-class PostgresPoliciesAPI:
-    """PostgreSQL
-
-    Encompasses methods used for all API and PDP interactions with our PostgreSQL DB.
-    Interacts with the snm_policies_pg (for bots' policies and PDP queries) database
-    """
-
-    def __init__(self):
-        log.debug("Connecting to PostgreSQL Policies")
-        try:
-            # Connect to the PostgreSQL server
-            self.conn = psycopg2.connect(host=credentials.POSTGRES_URL, database=credentials.POSTGRES_POLICIES_DB,
-                                         user=credentials.POSTGRES_USERNAME, password=credentials.POSTGRES_PASSWORD)
-
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-
-
-    def search_tweet(self, params=None):
-        """
-        Searches and returns all Tweets if no data is specified, or the specific tweet matching the given parameters
-
-        @param params: The parameters we want to query
-        @return The query's result or error
-        """
-
-        try:
-            cursor = self.conn.cursor()
-
-            query = "select timestamp, tweet_id, likes, retweets from tweets "
-            if params == None:
-                query += ";"
-            else:
-                query += "WHERE "
-                control = 0
-                if "tweet_id" in params.keys():
-                    query += "tweet_id = " + params["tweet_id"]
-                    control = 1
-                if "likes" in params.keys():
-                    if control == 1:
-                        query += " AND "
-                    query += "likes = " + params["likes"]
-                    control = 1
-                if "retweets" in params.keys():
-                    if control == 1:
-                        query += " AND "
-                    query += "retweets = " + params["retweets"]
-
-                query += ";"
-
-            cursor.execute(query)
-
-            data = cursor.fetchall()
-
-            self.conn.commit()
-            cursor.close()
-
-            # result = self.postProcessResults(data, ['timestamp', 'tweet_id', 'likes', 'retweets'])
-            result = []  # Array of jsons
-            for tuple in data:
-                result.append({"timestamp": tuple[0], "tweet_id": tuple[1], "likes": tuple[2], "retweets": tuple[3]})
-
-            return {"success": True, "data": result}
-        except psycopg2.Error as error:
-            self.conn.rollback()
-            return {"success": False, "error": error}
-
-
 if __name__ == "__main__":
     # TODO: Test and implement searching by timestamp ; Policies API
-    anal = PostgresAnalysisAPI()
+    anal = PostgresAPI()
     # anal.insert_tweet({"tweet_id": 831606548300517377, "user_id": 6253282, "likes": 100, "retweets": 2})
     # anal.insert_user({"user_id": 6253283, "followers": 10000, "following": 1234})
-    try:
-        for i in anal.search_tweet()["data"]:
-            print(i)
-    except:
-        print(anal.search_tweet()["error"])
+    #try:
+    #    for i in anal.search_tweet()["data"]:
+    #        print(i)
+    #except:
+    #    print(anal.search_tweet()["error"])
 
-    try:
-        for i in anal.search_user()["data"]:
-            print(i)
-    except:
-        print(anal.search_tweet()["error"])
-
-    # print(datetime.datetime.now())
+    #try:
+    #    for i in anal.search_user()["data"]:
+    #        print(i)
+    #except:
+    #    print(anal.search_tweet()["error"])
+    
