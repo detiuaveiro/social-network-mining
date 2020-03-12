4dc94a4b81769424dfbb7da9dfa054c03c5f6d6c
escaleira <pedroescaleira@hotmail.com>
2020-03-12 20:56:49 +0000

    small fix

--- code/backend/twitter/rest/run_tests.sh
+++ code/backend/twitter/rest/run_tests.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #echo "Running Mongo tests..."
 #pytest api/tests/mongo_tests/* -o DJANGO_SETTINGS_MODULE=rest.mongodb_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
 echo "Running Postgres tests..."
