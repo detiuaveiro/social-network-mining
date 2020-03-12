18d67c73ccb9cebc4811c4b561b9a6492bcd5d53
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-12 19:12:51 +0000

    bots test done

--- code/backend/twitter/rest/run_tests.sh
+++ code/backend/twitter/rest/run_tests.sh
@@ -1,4 +1,4 @@
 echo "Running Mongo tests..."
-pytest api/tests/mongo_tests/* -o DJANGO_SETTINGS_MODULE=rest.mongodb_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
-#echo "Running Postgres tests..."
-#pytest api/tests/postgresql_tests/* -s -o DJANGO_SETTINGS_MODULE=rest.postgres_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
+pytest api/tests/mongo_tests/* -s -o DJANGO_SETTINGS_MODULE=rest.mongodb_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
+echo "Running Postgres tests..."
+pytest api/tests/postgresql_tests/* -s -o DJANGO_SETTINGS_MODULE=rest.postgres_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
