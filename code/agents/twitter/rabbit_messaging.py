c92da6857acbf28b427d5d12cf93992dbbd5827f
escaleira <pedroescaleira@hotmail.com>
2020-03-12 16:20:39 +0000

    rabbitmq class almoust done

--- code/agents/twitter/rabbit_messaging.py
+++ code/agents/twitter/rabbit_messaging.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict
 from pyrabbit2.http import NetworkError
 from pyrabbit2.api import Client
 
@@ -8,11 +9,25 @@ logger = logging.getLogger("rabbit-messaging")
 logger.setLevel(logging.DEBUG)
 
 
+class MessagingSettings:
+	def __init__(self, exchange, queue=None, routing_key=None):
+		self.exchange = exchange
+		self.queue = queue
+		self.routing_key = routing_key
+
+	def __str__(self) -> str:
+		return f"exchange={self.exchange}, queue={self.queue}, routing_key={self.routing_key}"
+
+
 class RabbitMessaging:
-	def __init__(self, url, username, password, reconnect_max_iterations=5):
+	def __init__(self, url, username, password, vhost, settings: Dict[str, MessagingSettings], reconnect_max_iterations=5):
 		self.__url = url
 		self.__username = username
 		self.__password = password
+
+		#
+		self.vhost = vhost
+		self.settings = settings
 
 		self._messaging: Client
 
@@ -27,65 +42,41 @@ class RabbitMessaging:
 		:param function: function to decorate
 		"""
 		def wrapper(self, *args, **kwargs):
-			for current_reconnect in range(self.reconnect_max_iterations):
+			for current_reconnect in range(self.__reconnect_max_iterations):
 				try:
 					return function(self, *args, **kwargs)
 				except NetworkError as error:
 					logger.error(f"{current_time()}: Connection to RabbitMQ lost. Trying to reconnect...")
 					self.__connect()
 
-					if current_reconnect == self.reconnect_max_iterations - 1:
+					if current_reconnect == self.__reconnect_max_iterations - 1:
 						raise error
 		return wrapper
 
 	@__reconnect_messaging
 	def __setup_messaging(self):
+		"""Private method for setting up the messaging connections
 		"""
-			Private method for setuping the messaging connections
-		"""
-		print("ola")
-		# logger.debug("Setting up Messaging to: Receive Tasks")
-		# logger.debug(f"Connecting to exchange {self.tasks_exchange}")
-		# self.messaging.create_exchange(vhost=self.vhost, name=self.tasks_exchange, xtype="direct")
-		# logger.debug(f"Creating queue {self.tasks_queue}")
-		# self.messaging.create_queue(vhost=self.vhost, name=self.tasks_queue, durable=True)
-		# logger.debug(f"Binding exchange to queue {self.tasks_queue} with key {self.tasks_routing_key}")
-		# self.messaging.create_binding(vhost=self.vhost, exchange=self.tasks_exchange,
-		# 							  queue=self.tasks_queue, rt_key=self.tasks_routing_key)
-		# logger.info("---------------------------------------")
-		# logger.info(
-		# 	f"Connected to Messaging Service using: exchange={self.tasks_exchange}, queue={self.tasks_queue}, routing_key={self.tasks_routing_key}")
-		# logger.info("Ready to receive tasks")
-#
-		# logger.debug("---------------------------------------")
-		# logger.debug("Setting up Messaging to: Upload Logs")
-		# logger.debug(f"Connecting to exchange {self.logger_exchange}")
-		# self.messaging.create_exchange(vhost=self.vhost, name=self.logger_exchange, xtype="direct")
-		# logger.info("---------------------------------------")
-		# logger.info(
-		# 	f"Connected to Messaging Service using: exchange={self.logger_exchange}")
-		# logger.info("Ready to: Upload Logs")
-#
-		# logger.debug("---------------------------------------")
-		# logger.debug("Setting up Messaging to: Make Queries")
-		# logger.debug(f"Connecting to exchange {self.query_exchange}")
-		# self.messaging.create_exchange(vhost=self.vhost, name=self.query_exchange, xtype="direct")
-		# logger.info("---------------------------------------")
-		# logger.info(
-		# 	f"Connected to Messaging Service using: exchange={self.query_exchange}")
-		# logger.info("Ready to: Make Queries")
-#
-		# logger.debug("---------------------------------------")
-		# logger.debug("Setting up Messaging to: Upload Data")
-		# logger.debug(f"Connecting to exchange {self.data_exchange}")
-		# self.messaging.create_exchange(vhost=self.vhost, name=self.data_exchange, xtype="direct")
-		# logger.info("---------------------------------------")
-		# logger.info(
-		# 	f"Connected to Messaging Service using: exchange={self.data_exchange}")
-		# logger.info("Ready to: Upload Data")
+		for current_setting_name in self.settings:
+			current_setting = self.settings[current_setting_name]
+
+			logger.info(f"Setting up Messaging to: {current_setting}\n"
+						 f"Connecting to exchange {current_setting.exchange}")
+			self.messaging.create_exchange(vhost=self.vhost, name=current_setting.exchange, xtype="direct")
+
+			if current_setting.queue and current_setting.routing_key:
+				logger.info(f"Creating queue {current_setting.queue}")
+				self.messaging.create_queue(vhost=self.vhost, name=current_setting.queue, durable=True)
+
+				logger.info(f"Binding exchange to queue {current_setting.queue} with key {current_setting.routing_key}")
+				self.messaging.create_binding(vhost=self.vhost, exchange=current_setting.exchange,
+											  queue=current_setting.queue, rt_key=current_setting.routing_key)
+
+			logger.info(f"Connected to Messaging Service using: {current_setting.__str__()}")
+			logger.info("---------------------------------------")
 
 	def __connect(self):
-		self.messaging = Client(api_url=self.url, user=self.username, passwd=self.password)
+		self.messaging = Client(api_url=self.__url, user=self.__username, passwd=self.__password)
 
 	def run(self):
 		self.__setup_messaging()
--- code/agents/twitter/twitter_bot.py
+++ code/agents/twitter/twitter_bot.py
@@ -4,7 +4,7 @@ import tweepy
 from tweepy.error import TweepError
 
 import messages_types
-from rabbit_messaging import RabbitMessaging
+from rabbit_messaging import RabbitMessaging, MessagingSettings
 from settings import *
 from utils import *
 
@@ -13,8 +13,8 @@ logger.setLevel(logging.DEBUG)
 
 
 class TwitterBot(RabbitMessaging):
-	def __init__(self, url, username, password, bot_id, api: tweepy.API):
-		super().__init__(url, username, password)
+	def __init__(self, url, username, password, vhost, messaging_settings, bot_id, api: tweepy.API):
+		super().__init__(url, username, password, vhost, messaging_settings)
 		self._id = bot_id
 		self._twitter_api = api
 		# self.user: User
@@ -39,7 +39,16 @@ class TwitterBot(RabbitMessaging):
 
 			self._messaging.publish(vhost=VHOST, xname=LOG_EXCHANGE, rt_key=LOG_ROUTING_KEY, payload=to_json(data))
 
+	def run(self):
+		super().run()
+
 
 if __name__ == "__main__":
-	bot = TwitterBot("URL", RABBIT_USERNAME, RABBIT_PASSWORD, 1, None)
+	messaging_settings = {
+		'tasks': MessagingSettings(TASKS_EXCHANGE, TASKS_QUEUE_PREFIX, TASKS_ROUTING_KEY_PREFIX),
+		'log': MessagingSettings(LOG_EXCHANGE, routing_key=LOG_ROUTING_KEY),
+		'query': MessagingSettings(QUERY_EXCHANGE, routing_key=QUERY_ROUTING_KEY),
+		'data': MessagingSettings(DATA_EXCHANGE, routing_key=DATA_ROUTING_KEY)
+	}
+	bot = TwitterBot("URL", RABBIT_USERNAME, RABBIT_PASSWORD, VHOST, messaging_settings, 1, None)
 	bot.run()
