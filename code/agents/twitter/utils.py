7f25e7c67f5d0ef9d36916edffed04400448f0d8
escaleira <pedroescaleira@hotmail.com>
2020-03-13 14:36:08 +0000

    something

--- code/agents/twitter/utils.py
+++ code/agents/twitter/utils.py
@@ -14,5 +14,7 @@ def from_json(string):
     return json.loads(string)
 
 
-def current_time():
+def current_time(str_time=False):
+    if str_time:
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return datetime.utcnow().timestamp()
