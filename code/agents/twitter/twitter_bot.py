66d757f20c4318e09c6aac6cbb7e34bddd6a89a5
escaleira <pedroescaleira@hotmail.com>
2020-03-13 23:58:17 +0000

    more work

--- code/agents/twitter/twitter_bot.py
+++ code/agents/twitter/twitter_bot.py
@@ -23,7 +23,7 @@ class TwitterBot(RabbitMessaging):
 	def __repr__(self):
 		return f"<TwitterBot id={self._id}, api={self._twitter_api}>"
 
-	def _send_error_suspended_log(self, error: TweepError):
+	def __send_error_suspended_log(self, error: TweepError):
 		logger.error(f"TweepyError with code=<{error.api_code}> and reason=<{error.reason}>: {error}")
 
 		if error.api_code in ACCOUNT_SUSPENDED_ERROR_CODES:
@@ -39,8 +39,19 @@ class TwitterBot(RabbitMessaging):
 
 			self._messaging.publish(vhost=VHOST, xname=LOG_EXCHANGE, rt_key=LOG_ROUTING_KEY, payload=to_json(data))
 
+	def __setup(self):
+		"""Function to setting up messaging queues and check if twitter credentials are ok
+		"""
+
+		logger.debug("Setting up messaging")
+		self._setup_messaging()
+
+		logger.debug("Verifying credentials")
+		self.user = self._twitter_api.verify_credentials()
+		print(self.user)
+
 	def run(self):
-		super().run()
+		self.__setup()
 
 
 if __name__ == "__main__":
@@ -50,5 +61,14 @@ if __name__ == "__main__":
 		'query': MessagingSettings(exchange=QUERY_EXCHANGE, routing_key=QUERY_ROUTING_KEY),
 		'data': MessagingSettings(exchange=DATA_EXCHANGE, routing_key=DATA_ROUTING_KEY)
 	}
-	bot = TwitterBot("localhost:15672", RABBIT_USERNAME, RABBIT_PASSWORD, VHOST, messaging_settings, 1, None)
+
+	consumer_key = "yqoymTNrS9ZDGsBnlFhIuw"
+	consumer_secret = "OMai1whT3sT3XMskI7DZ7xiju5i5rAYJnxSEHaKYvEs"
+	token = "1097916541830680576-RWAa8hM2tkGMXQWaa0Bg5sDYTFD0oV"
+	token_secret = "bYyREitpd1J1wr758FMwmk7TI5KHyMEomEv80jgecJUVL"
+
+	twitter_auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
+	twitter_auth.set_access_token(key=token, secret=token_secret)
+	bot = TwitterBot("localhost:15672", RABBIT_USERNAME, RABBIT_PASSWORD, VHOST, messaging_settings, 1,
+					 tweepy.API(auth_handler=twitter_auth, wait_on_rate_limit=True))
 	bot.run()
