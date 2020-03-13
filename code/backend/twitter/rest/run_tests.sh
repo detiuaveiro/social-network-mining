51e12239e1e6df5a9f591e0cd0fe3efd48a27198
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-13 19:49:36 +0000

    k

--- code/backend/twitter/rest/run_tests.sh
+++ code/backend/twitter/rest/run_tests.sh
@@ -3,4 +3,4 @@
 #echo "Running Mongo tests..."
 #pytest api/tests/mongo_tests/* -o DJANGO_SETTINGS_MODULE=rest.mongodb_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
 echo "Running Postgres tests..."
-pytest api/tests/postgresql_tests/* -s -o DJANGO_SETTINGS_MODULE=rest.postgres_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
+pytest api/tests/postgresql_tests/test_policies.py -s -o DJANGO_SETTINGS_MODULE=rest.postgres_test_settings -o addopts=--cov=api/views --cov-report  html --reuse-db
