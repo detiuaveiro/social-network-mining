49321739e76224150643dc27f5c1e03711da343c
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-14 18:01:16 +0000

    Tests without stderr

--- code/backend/twitter/rest/rest/mongodb_test_settings.py
+++ code/backend/twitter/rest/rest/mongodb_test_settings.py
@@ -1,13 +1,13 @@
 from rest.settings import *
-
+import os
 
 DATABASE_ROUTERS = ["api.tests.mongo_tests.mongo_db_router.DB_Router"]
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'twitter',
-        'HOST': '127.0.0.1',
-        'PORT': 27017,
+        'HOST': 'mongo',
+        'PORT': int(os.environ['MONGO_PORT']),
         'USER': 'admin',
         'PASSWORD': 'admin',
     },
