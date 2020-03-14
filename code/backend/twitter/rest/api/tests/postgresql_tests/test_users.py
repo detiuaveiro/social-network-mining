79d4939caa6be1b9ae307d44c767fd9af9412836
Rafaelyot <rafaeljsimoes@gmail.com>
2020-03-14 01:17:49 +0000

    Users test done

---
+++ code/backend/twitter/rest/api/tests/postgresql_tests/test_users.py
@@ -0,0 +1,51 @@
+import pytest
+from django.test import RequestFactory
+from django.urls import reverse
+from api.models import UserStats
+from api.views import users
+from mixer.backend.django import mixer
+from api.tests.utils import *
+from api import neo4j
+
+
+@pytest.fixture(scope='module')
+def factory():
+    return RequestFactory()
+
+
+@pytest.fixture
+def users_stats(db):
+    return mixer.cycle(5).blend(UserStats)
+
+
+@pytest.fixture
+def user_stats(db):
+    return mixer.blend(UserStats, id=1, user_id=1)
+
+
+def test_successful_twitter_users_stats_request(factory, users_stats):
+    path = reverse('twitter_users_stats')
+    request = factory.get(path)
+    response = users.twitter_users_stats(request)
+    assert is_response_successful(response)
+
+
+def test_unsuccessfully_twitter_users_stats_request(factory, db):
+    path = reverse('twitter_users_stats')
+    request = factory.get(path)
+    response = users.twitter_users_stats(request)
+    assert is_response_empty(response)
+
+
+def test_successful_twitter_user_stats_request(factory, user_stats):
+    path = reverse('twitter_user_stats', kwargs={'id': 1})
+    request = factory.get(path)
+    response = users.twitter_user_stats(request, id=1)
+    assert is_response_successful(response)
+
+
+def test_unsuccessfully_twitter_user_stats_request(factory, db):
+    path = reverse('twitter_user_stats', kwargs={'id': 1})
+    request = factory.get(path)
+    response = users.twitter_user_stats(request, id=1)
+    assert is_response_unsuccessful(response)
