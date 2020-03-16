5af1eeff30cb7d3294415d4143b7b855100dad18
escaleira <pedroescaleira@hotmail.com>
2020-03-16 06:23:54 +0000

    follow users done

--- code/agents/twitter/settings.py
+++ code/agents/twitter/settings.py
@@ -23,13 +23,14 @@ DATA_ROUTING_KEY = "data.twitter"
 # -----------------------------------------------------------
 
 ACCOUNT_SUSPENDED_ERROR_CODES = [63, 64, 326]
+FOLLOW_USER_ERROR_CODE = 161
 
 
 # -----------------------------------------------------------
 # Tweepy settings
 # -----------------------------------------------------------
 
-MAX_NUMBER_TWEETS_RETRIEVE_TIMELINE = 10
+MAX_NUMBER_TWEETS_RETRIEVE_TIMELINE = 1
 
 
 # -----------------------------------------------------------
