6d5299927ec044e5fbbea09c1d4ab0ffa95d6919
escaleira <pedroescaleira@hotmail.com>
2020-03-17 07:30:48 +0000

    fixes on the way we collect the environment variables

--- code/backend/twitter/credentials.py
+++ code/backend/twitter/credentials.py
@@ -3,33 +3,33 @@ import os
 # -----------------------------------------------------------
 #  MONGO 
 # -----------------------------------------------------------
-MONGO_URL = os.environ['MONGO_URL']  # "localhost"
+MONGO_URL = os.environ.get('MONGO_URL', 'localhost')
 MONGO_PORT = 27017
 MONGO_FULL_URL = f"{MONGO_URL}:{MONGO_PORT}"
-MONGO_DB = os.environ['MONGO_DB']  # "twitter"
-MONGO_USERNAME = os.environ['MONGO_USERNAME']  # "admin"
-MONGO_PASSWORD = os.environ['MONGO_PASSWORD']  # "admin"
+MONGO_DB = os.environ.get('MONGO_DB', 'twitter')
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'admin')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'admin')
 
 # -----------------------------------------------------------
 # POSTGRES 
 # -----------------------------------------------------------
-POSTGRES_URL = os.environ['POSTGRES_URL']  # "localhost"
+POSTGRES_URL = os.environ.get('POSTGRES_URL', 'localhsot')
 POSTGRES_PORT = 5432
 POSTGRES_FULL_URL = f"{POSTGRES_URL}:{POSTGRES_PORT}"
-POSTGRES_DB = os.environ['POSTGRES_DB']  # "twitter"
-POSTGRES_USERNAME = os.environ['POSTGRES_USERNAME']  # "admin"
-POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']  # "admin"
+POSTGRES_DB = os.environ.get('POSTGRES_DB', 'twitter')
+POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME', 'admin')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'admin')
 
 # -----------------------------------------------------------
 # RABBITMQ
 # -----------------------------------------------------------
-RABBITMQ_URL = os.environ['RABBITMQ_URL']  # "localhost"
+RABBITMQ_URL = os.environ.get('RABBITMQ_URL', 'localhost')
 RABBITMQ_PORT = 5672
 RABBITMQ_HTTP_PORT = 15672
 RABBITMQ_FULL_URL = f"{RABBITMQ_URL}:{RABBITMQ_PORT}"
 RABBITMQ_FULL_HTTP_URL = f"{RABBITMQ_URL}:{RABBITMQ_HTTP_PORT}"
-RABBITMQ_USERNAME = os.environ['RABBITMQ_USERNAME']  # "pi_rabbit_admin"
-RABBITMQ_PASSWORD = os.environ['RABBITMQ_PASSWORD']  # "yPvawEVxks7MLg3lfr3g"
+RABBITMQ_USERNAME = os.environ.get('RABBITMQ_USERNAME', 'admin')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'admin')
 
 # Queues and Exchanges
 VHOST = "PI"
@@ -53,8 +53,8 @@ API_QUEUE = 'API'
 # -----------------------------------------------------------
 # NEO4J
 # -----------------------------------------------------------
-NEO4J_URL = os.environ['NEO4J_URL']  # "localhost"
+NEO4J_URL = os.environ.get('NEO4J_URL', 'localhost')
 NEO4J_PORT = 7687
 NEO4J_FULL_URL = f"{NEO4J_URL}:{NEO4J_PORT}"
-NEO4J_USERNAME = os.environ['NEO4J_USERNAME']  # "neo4j"
-NEO4J_PASSWORD = os.environ['NEO4J_PASSWORD']  # "neo4jPI"
+NEO4J_USERNAME = os.environ.get('NEO4J_USERNAME', 'neo4j')
+NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD', 'neo4jPI')
--- code/backend/twitter/start_bot.py
+++ code/backend/twitter/start_bot.py
@@ -8,7 +8,7 @@ from credentials import TASKS_EXCHANGE, TASKS_ROUTING_KEY_PREFIX, TASKS_QUEUE_PR
 	RABBITMQ_FULL_HTTP_URL
 
 if __name__ == "__main__":
-	bot_id = os.environ['BOT_ID']
+	bot_id = os.environ.get('BOT_ID')
 
 	messaging_settings = {
 		TASKS_EXCHANGE: MessagingSettings(exchange=TASKS_EXCHANGE, routing_key=f"{TASKS_ROUTING_KEY_PREFIX}.{bot_id}",
@@ -18,10 +18,10 @@ if __name__ == "__main__":
 		DATA_EXCHANGE: MessagingSettings(exchange=DATA_EXCHANGE, routing_key=DATA_ROUTING_KEY)
 	}
 
-	consumer_key = os.environ['CONSUMER_KEY']
-	consumer_secret = os.environ['CONSUMER_SECRET']
-	token = os.environ['TOKEN']
-	token_secret = os.environ['TOKEN_SECRET']
+	consumer_key = os.environ.get('CONSUMER_KEY')
+	consumer_secret = os.environ.get('CONSUMER_SECRET')
+	token = os.environ.get('TOKEN')
+	token_secret = os.environ.get('TOKEN_SECRET')
 
 	twitter_auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
 	twitter_auth.set_access_token(key=token, secret=token_secret)
