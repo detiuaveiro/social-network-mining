1edafa46dd6f7803d24bf5fc11beaae072f2c24f
HerouFenix <diogo04@ua.pt>
2020-03-29 17:31:30 +0100

    changed routes ,started preparing landing page

---
+++ code/web-app/src/containers/LandingPageLayout/LandingLayout.js
@@ -0,0 +1,14 @@
+import React, { Component, Suspense } from "react";
+
+class LandingLayout extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingLayout;
