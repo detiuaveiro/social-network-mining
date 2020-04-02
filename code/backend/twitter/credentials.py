4dda6b0ba0805c1c211049ade14f9d82574f8123
Rafaelyot <rafaeljsimoes@gmail.com>
2020-04-02 04:48:15 +0100

    credentials updated

--- code/backend/twitter/credentials.py
+++ code/backend/twitter/credentials.py
@@ -5,10 +5,10 @@ import os
 # -----------------------------------------------------------
 MONGO_URL = os.environ.get('MONGO_URL', 'localhost')
 MONGO_PORT = 27017
-MONGO_FULL_URL = f"{MONGO_URL}:{MONGO_PORT}"
 MONGO_DB = os.environ.get('MONGO_DB', 'twitter')
 MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'admin')
 MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'admin')
+MONGO_FULL_URL = f"{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_URL}:{MONGO_PORT}"
 
 # -----------------------------------------------------------
 # POSTGRES 
