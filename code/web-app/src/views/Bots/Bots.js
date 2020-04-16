1c03b5b24cc305e5ccf624b5efa283774b2cd008
HerouFenix <diogo04@ua.pt>
2020-04-16 18:09:00 +0100

    continuing work, added error message

--- code/web-app/src/views/Bots/Bots.js
+++ code/web-app/src/views/Bots/Bots.js
@@ -11,106 +11,172 @@ import CardFooter from "../../components/Card/CardFooter";
 import CardIcon from "../../components/Card/CardIcon";
 
 class Bots extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
   }
 
+  state = {
+    admin: false,
+    bots: [],
+  };
+
+  componentDidMount() {
+    fetch(baseURL + "twitter/bots", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then(response => {
+      if (response.ok) return response.json();
+      else {
+        throw new Error(response.status);
+      }
+    }).then(data => {
+      console.log(data)
+    }).catch(error => {
+      console.log("error: " + error);
+      this.setState({
+        error: true,
+        bots: []
+      })
+    });
+  }
 
   loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
 
   render() {
-
-    return (
-      <div className="animated fadeIn">
-        <Container fluid>
-          <Row>
-            <Col xs="12" sm="12" md="9">
-              <Card>
-                <CardHeader color="primary">
-                  <h3 style={{ color: "white" }}>
-                    <strong>Bots</strong>
-                  </h3>
-                  <h5 style={{ color: "white" }}>
-                    List with all registered bot accounts and their status
+    if (this.state.error) {
+      return (
+        <div className="animated fadeIn">
+          <Container fluid>
+            <Row>
+              <Col xs="12" sm="12" md="9">
+                <Card>
+                  <CardHeader color="primary">
+                    <h3 style={{ color: "white" }}>
+                      <strong>Bots</strong>
+                    </h3>
+                    <h5 style={{ color: "white" }}>
+                      List with all registered bot accounts and their status
                   </h5>
-                </CardHeader>
-                <CardBody>
-                </CardBody>
-              </Card>
-            </Col>
+                  </CardHeader>
+                  <CardBody>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="12" sm="12" md="12">
+                <Card>
+                  <CardHeader color="primary">
+                    <h4 style={{
+                      color: "#FFFFFF",
+                      marginTop: "0px",
+                      minHeight: "auto",
+                      marginBottom: "3px",
+                      textDecoration: "none",
+                      "& small": {
+                        color: "#777",
+                        fontSize: "65%",
+                        fontWeight: "400",
+                        lineHeight: "1"
+                      }
+                    }} > Registered Accounts</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <p>Sorry, an error has occured! Please try again shortly</p>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div >
+      )
+    } else {
+      return (
+        <div className="animated fadeIn">
+          <Container fluid>
+            <Row>
+              <Col xs="12" sm="12" md="9">
+                <Card>
+                  <CardHeader color="primary">
+                    <h3 style={{ color: "white" }}>
+                      <strong>Bots</strong>
+                    </h3>
+                    <h5 style={{ color: "white" }}>
+                      List with all registered bot accounts and their status
+                    </h5>
+                  </CardHeader>
+                  <CardBody>
+                  </CardBody>
+                </Card>
+              </Col>
 
-            <Col xs="12" sm="12" md="3">
-              <Card>
-                <CardHeader color="primary" stats icon>
-                  <CardIcon color="primary">
-                    <i class="fab fa-twitter"></i>
-                  </CardIcon>
-                  <p style={{
-                    color: '#999',
-                    margin: "0",
-                    fontSize: "14px",
-                    marginTop: "0",
-                    paddingTop: "10px",
-                    marginBottom: "0"
-                  }}>Active accounts</p>
-                  <h3 style={{
-                    color: "#23282c",
-                    minHeight: "auto",
-                    marginBottom: "3px",
-                    "& small": {
-                      color: "#777",
-                      fontWeight: "400",
-                      lineHeight: "1"
-                    }
-                  }} >
-                    49 / 50 < small > Bots</small>
-                  </h3>
-                </CardHeader>
-                <CardBody style={{ minHeight: "38px" }}>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="12" sm="12" md="12">
-              <Card>
-                <CardHeader color="primary">
-                  <h4 style={{
-                    color: "#FFFFFF",
-                    marginTop: "0px",
-                    minHeight: "auto",
-                    marginBottom: "3px",
-                    textDecoration: "none",
-                    "& small": {
-                      color: "#777",
-                      fontSize: "65%",
-                      fontWeight: "400",
-                      lineHeight: "1"
-                    }
-                  }} > Registered Accounts</h4>
-                  <Button block outline color="light" style={{width:"10%"}}>Add new</Button>
-                </CardHeader>
-              <CardBody>
-                <Table
-                  tableHeaderColor="primary"
-                  tableHead={["Username", "Name", "other", "shit"]}
-                  tableData={[
-                    ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                    ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                    ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                    ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                    ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                    ["Mason Porter", "Chile", "Gloucester", "$78,615"]
-                  ]}
-                />
-              </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-
-      </div >
-    );
+              <Col xs="12" sm="12" md="3">
+                <Card>
+                  <CardHeader color="primary" stats icon>
+                    <CardIcon color="primary">
+                      <i class="fab fa-twitter"></i>
+                    </CardIcon>
+                    <p style={{
+                      color: '#999',
+                      margin: "0",
+                      fontSize: "14px",
+                      marginTop: "0",
+                      paddingTop: "10px",
+                      marginBottom: "0"
+                    }}>Active accounts</p>
+                    <h3 style={{
+                      color: "#23282c",
+                      minHeight: "auto",
+                      marginBottom: "3px",
+                      "& small": {
+                        color: "#777",
+                        fontWeight: "400",
+                        lineHeight: "1"
+                      }
+                    }} >
+                      49 / 50 < small > Bots</small>
+                    </h3>
+                  </CardHeader>
+                  <CardBody style={{ minHeight: "38px" }}>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="12" sm="12" md="12">
+                <Card>
+                  <CardHeader color="primary">
+                    <h4 style={{
+                      color: "#FFFFFF",
+                      marginTop: "0px",
+                      minHeight: "auto",
+                      marginBottom: "3px",
+                      textDecoration: "none",
+                      "& small": {
+                        color: "#777",
+                        fontSize: "65%",
+                        fontWeight: "400",
+                        lineHeight: "1"
+                      }
+                    }} > Registered Accounts</h4>
+                    <Button block outline color="light" style={{ width: "10%" }}>Add new</Button>
+                  </CardHeader>
+                  <CardBody>
+                    <Table
+                      tableHeaderColor="primary"
+                      tableHead={["Username", "Name", "Followers", "Following", ""]}
+                      tableData={[]}
+                    />
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div >
+      );
+    }
   }
 }
 
