9a816e43490e3dd1f9ed38c07632470b4ac81c9d
escaleira <pedroescaleira@hotmail.com>
2020-04-18 04:38:48 +0100

    some minor fixes

--- code/backend/twitter/bots/twitter_bot.py
+++ code/backend/twitter/bots/twitter_bot.py
@@ -141,7 +141,7 @@ class TwitterBot(RabbitMessaging):
 			return list(self._twitter_api.home_timeline(**kwargs))
 		return list(user.timeline(**kwargs))
 
-	def __read_timeline(self, user: User, jump_users: bool = False, max_depth: int = 5, current_depth: int = 0):
+	def __read_timeline(self, user: User, jump_users: bool = False, max_depth: int = 3, current_depth: int = 0):
 		"""Function to get an user's timeline tweets. At the same time, this function send to the server requests to
 			like a tweet, to retweet a tweet and also sends all users that made some tweet, but we don't know yet.
 			Also, this function is recursive, and tries to get all this data to the new users it possibility find
