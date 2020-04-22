1dfac70efd2d509d71172e735b805df81415289e
DrPunpun <pedrooliveira99@ua.pt>
2020-04-22 14:48:31 +0100

    wrappers test for tweet

---
+++ code/backend/twitter/test.py
@@ -0,0 +1,34 @@
+from wrappers.neo4j_wrapper import Neo4jAPI
+
+if __name__ == "__main__":
+	neo = Neo4jAPI()
+
+	neo.add_user({"id": 0, "name": "DS", "username": "FenixD.S"})
+	neo.add_tweet({"id": 1})
+	neo.add_writer_relationship({"tweet_id": 1, "user_id": 0, "user_type": "User"})
+
+	neo.add_user({"id": 2, "name": "Jonas", "username": "Jonas_Pistolas"})
+	neo.add_tweet({"id": 3})
+	neo.add_writer_relationship({"tweet_id": 3, "user_id": 2, "user_type": "User"})
+
+	neo.add_retweet_relationship({"tweet_id": 1, "user_id": 2, "user_type": "User"})
+	neo.add_reply_relationship({"tweet": 1, "reply": 3})
+
+	print(neo.check_tweet_exists(1))
+	print(neo.check_tweet_exists(3))
+	print(neo.check_writer_relationship({"tweet_id": 3, "user_id": 2, "user_type": "User"}))
+	print(neo.check_retweet_relationship({"tweet_id": 1, "user_id": 2, "user_type": "User"}))
+	print(neo.check_reply_relationship({"tweet": 1, "reply": 3}))
+
+	neo.delete_user(0)
+	neo.delete_tweet(1)
+	neo.delete_writer_relationship({"tweet_id": 1, "user_id": 0, "user_type": "User"})
+
+	neo.delete_user(2)
+	neo.delete_tweet(3)
+	neo.delete_writer_relationship({"tweet_id": 3, "user_id": 2, "user_type": "User"})
+
+	neo.delete_retweet_relationship({"tweet_id": 1, "user_id": 2, "user_type": "User"})
+	neo.delete_reply_relationship({"tweet": 1, "reply": 3})
+
+	print(neo.get_tweet_network(1))
