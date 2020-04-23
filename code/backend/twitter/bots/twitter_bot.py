476e42e6cb72bb2d2f5cc6a44d8fc8e1db390387
escaleira <pedroescaleira@hotmail.com>
2020-04-23 12:27:32 +0100

    fixes

--- code/backend/twitter/bots/twitter_bot.py
+++ code/backend/twitter/bots/twitter_bot.py
@@ -408,8 +408,8 @@ class TwitterBot(RabbitMessaging):
 					if task_type == messages_types.ServerToBot.FIND_BY_KEYWORDS:
 						logger.warning(
 							f"Not processing {messages_types.ServerToBot.FIND_BY_KEYWORDS} with {task_params}")
-					# elif task_type == messages_types.ServerToBot.FOLLOW_USERS:
-					# 	self.__follow_users(id_type=task_params['type'], data=task_params['data'])
+					elif task_type == messages_types.ServerToBot.FOLLOW_USERS:
+						self.__follow_users(id_type=task_params['type'], data=task_params['data'])
 					elif task_type == messages_types.ServerToBot.LIKE_TWEETS:
 						self.__like_tweet(task_params)
 					elif task_type == messages_types.ServerToBot.RETWEET_TWEETS:
@@ -426,6 +426,11 @@ class TwitterBot(RabbitMessaging):
 						logger.warning(f"Received unknown task type: {task_type}")
 				else:
 					logger.warning("There are not new messages on the tasks's queue")
+
+					logger.info("Update the control center with the users who follow us")
+					self.__get_followers(user_id=self._id)
+
+					logger.info("Ask control center for keywords to search new tweets")
 					self.__send_user(self._twitter_api.me(), messages_types.BotToServer.QUERY_KEYWORDS)
 					wait(2)
 			except Exception as error:
