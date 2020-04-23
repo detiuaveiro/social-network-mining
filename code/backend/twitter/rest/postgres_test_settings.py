0b306c850f153df1d9f647c59f6a0d7aed2e81a5
Rafaelyot <rafaeljsimoes@gmail.com>
2020-04-24 00:56:00 +0100

    Tests fix

--- code/backend/twitter/rest/postgres_test_settings.py
+++ code/backend/twitter/rest/postgres_test_settings.py
@@ -3,12 +3,12 @@ import os
 
 DATABASE_ROUTERS = ["api.tests.postgresql_tests.postgres_db_router.DB_Router"]
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'twitter_postgres',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': os.environ['POSTGRES_HOST'],
-        'PORT': os.environ['POSTGRES_PORT'],
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'twitter_postgres',
+		'USER': 'admin',
+		'PASSWORD': 'admin',
+		'HOST': os.environ['POSTGRES_HOST'],
+		'PORT': os.environ['POSTGRES_PORT'],
+	}
 }
