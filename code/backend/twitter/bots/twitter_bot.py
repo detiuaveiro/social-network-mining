b21f6046de813aa969fb3ccd1d96c732affda584
escaleira <pedroescaleira@hotmail.com>
2020-04-29 04:20:52 +0100

    fails on github pipeline that no one can ever understand

--- code/backend/twitter/bots/twitter_bot.py
+++ code/backend/twitter/bots/twitter_bot.py
@@ -472,4 +472,3 @@ class TwitterBot(RabbitMessaging):
                     wait(2)
             except Exception as error:
                 logger.exception(f"Error {error} on bot's loop: ")
-        # exit(1)
