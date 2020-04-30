68e17e55e32a63097e072aacce772024a7479659
escaleira <pedroescaleira@hotmail.com>
2020-04-30 20:56:42 +0100

    another try

--- code/backend/twitter/wrappers/rabbitmq_wrapper.py
+++ code/backend/twitter/wrappers/rabbitmq_wrapper.py
@@ -68,7 +68,7 @@ class Rabbitmq:
         self.connection = pika.BlockingConnection(self.pika_parameters)
 
         self.channel: BlockingChannel = self.connection.channel()
-        self.channel.basic_qos(prefetch_count=1, global_qos=True)
+        self.channel.basic_qos(prefetch_count=1, prefetch_size=1, global_qos=True)
         self.channel.queue_declare(queue=API_QUEUE, durable=True)
 
         # Declare Exchanges
