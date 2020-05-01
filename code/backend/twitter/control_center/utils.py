053cd06aad0dfb43649d31779786e7d1cf42649b
escaleira <pedroescaleira@hotmail.com>
2020-05-01 03:21:51 +0100

    fix requested

--- code/backend/twitter/control_center/utils.py
+++ code/backend/twitter/control_center/utils.py
@@ -2,5 +2,4 @@ import re
 
 
 def tweet_to_simple_text(tweet: str) -> str:
-	text_with_emojis = re.sub(r'@.*? |\n|http.*', '', tweet).encode('ascii', 'ignore').decode('ascii')
-	return text_with_emojis.encode('latin-1', 'ignore').decode('latin-1')
+	return re.sub(r'@.*? |\n|http.*', '', tweet).encode('latin-1', 'ignore').decode('latin-1')
