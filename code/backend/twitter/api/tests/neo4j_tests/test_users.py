79412d692175922d747eb415486fc4de7086da84
DrPunpun <pedrooliveira99@ua.pt>
2020-05-01 19:22:11 +0100

    coerrected stuff

--- code/backend/twitter/api/tests/neo4j_tests/test_users.py
+++ code/backend/twitter/api/tests/neo4j_tests/test_users.py
@@ -36,32 +36,32 @@ def add_relationship(id_1, id_2):
 @catch_exception
 def test_successful_twitter_user_followers_request(error_catcher, factory):
     assert add_bot_neo4j(["1", "2"]) and add_relationship("2", "1")
-    path = reverse('twitter_user_followers', kwargs={'id': 1})
+    path = reverse('twitter_user_followers', kwargs={'id': "1"})
     request = factory.get(path)
-    response = users.twitter_user_followers(request, id=1)
+    response = users.twitter_user_followers(request, id="1")
     assert is_response_successful(response)
 
 
 @catch_exception
 def test_empty_twitter_user_followers_request(error_catcher, factory):
-    path = reverse('twitter_user_followers', kwargs={'id': 1})
+    path = reverse('twitter_user_followers', kwargs={'id': "1"})
     request = factory.get(path)
-    response = users.twitter_user_followers(request, id=1)
+    response = users.twitter_user_followers(request, id="1")
     assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_successful_twitter_user_following_request(error_catcher, factory):
-    assert add_bot_neo4j([1, 2]) and add_relationship(1, 2)
-    path = reverse('twitter_user_following', kwargs={'id': 1})
+    assert add_bot_neo4j(["1", "2"]) and add_relationship("1", "2")
+    path = reverse('twitter_user_following', kwargs={'id': "1"})
     request = factory.get(path)
-    response = users.twitter_user_following(request, id=1)
+    response = users.twitter_user_following(request, id="1")
     assert is_response_successful(response)
 
 
 @catch_exception
 def test_empty_twitter_user_following_request(error_catcher, factory):
-    path = reverse('twitter_user_following', kwargs={'id': 1})
+    path = reverse('twitter_user_following', kwargs={'id': "1"})
     request = factory.get(path)
-    response = users.twitter_user_following(request, id=1)
+    response = users.twitter_user_following(request, id="1")
     assert is_response_unsuccessful(response)
