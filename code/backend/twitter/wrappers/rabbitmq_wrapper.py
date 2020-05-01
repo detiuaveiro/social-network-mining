04e3844c5c1e0e89456274a6ded1e31ab20e2548
escaleira <pedroescaleira@hotmail.com>
2020-05-01 05:42:18 +0100

    trying another configurations

--- code/backend/twitter/wrappers/rabbitmq_wrapper.py
+++ code/backend/twitter/wrappers/rabbitmq_wrapper.py
@@ -48,8 +48,8 @@ class Rabbitmq:
             port=port,
             virtual_host=vhost,
             credentials=pika_credentials,
-            heartbeat=600,
-            blocked_connection_timeout=300
+            heartbeat=60,
+            blocked_connection_timeout=30
         )
 
         self.reconnection_attempt = 0
@@ -67,7 +67,7 @@ class Rabbitmq:
         self.connection = pika.BlockingConnection(self.pika_parameters)
 
         self.channel: BlockingChannel = self.connection.channel()
-        self.channel.basic_qos(prefetch_count=1, global_qos=True)
+        self.channel.basic_qos(prefetch_count=10, global_qos=True)
         self.channel.queue_declare(queue=API_QUEUE, durable=True)
 
         # Declare Exchanges
