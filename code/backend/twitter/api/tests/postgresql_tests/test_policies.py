f4afa128219a99c039487e828b621e9e5d327cdf
Rafaelyot <rafaeljsimoes@gmail.com>
2020-05-04 02:03:23 +0100

    Tests fixed

--- code/backend/twitter/api/tests/postgresql_tests/test_policies.py
+++ code/backend/twitter/api/tests/postgresql_tests/test_policies.py
@@ -13,161 +13,185 @@ from api import neo4j
 
 @pytest.fixture(scope='module')
 def factory():
-    return RequestFactory()
+	return RequestFactory()
 
 
 @pytest.fixture
 def policy(db):
-    return mixer.blend(Policy, tags=["PSD", "PS"], bots=[1], id=1,
-                       API_type=choice([x[0] for x in Enum_policy.api_types()]),
-                       filter=choice([x[0] for x in Enum_policy.api_filter()]))
+	return mixer.blend(Policy, tags=["PSD", "PS"], bots=[1], id=1,
+					   API_type=choice([x[0] for x in Enum_policy.api_types()]),
+					   filter=choice([x[0] for x in Enum_policy.api_filter()]))
 
 
 @pytest.fixture
-def policy_twitter(db):
-    return mixer.blend(Policy, tags=["PSD", "PS"], bots=[1], id=1,
-                       API_type="Twitter", filter=choice([x[0] for x in Enum_policy.api_filter()]))
-
-
-@pytest.fixture
-def policy_instagram(db):
-    return mixer.blend(Policy, tags=["PSD", "PS"], bots=[1], id=1,
-                       API_type="Instagram", filter=choice([x[0] for x in Enum_policy.api_filter()]))
+def policies_list(db):
+	return mixer.cycle(20).blend(Policy, tags=["PSD", "PS"], bots=[1])
 
 
 def add_bot_neo4j(bot_id=1):
-    neo4j.add_bot({'id': bot_id, 'name': 'bot_test', 'username': 'bot_test_username'})
-    return neo4j.check_bot_exists(bot_id)
+	neo4j.add_bot({'id': bot_id, 'name': 'bot_test', 'username': 'bot_test_username'})
+	return neo4j.check_bot_exists(bot_id)
 
 
 @pytest.fixture(autouse=True)
 def delete_neo4j_data():
-    for bot_id in [1, 2]:
-        neo4j.delete_bot(bot_id)
-        if neo4j.check_bot_exists(bot_id):
-            return False
-    return True
+	for bot_id in [1, 2]:
+		neo4j.delete_bot(bot_id)
+		if neo4j.check_bot_exists(bot_id):
+			return False
+	return True
 
 
 @catch_exception
-def test_successful_policies_request(error_catcher, factory, policy):
-    path = reverse('policies')
-    request = factory.get(path)
-    response = policies.policies(request)
-    assert is_response_successful(response)
+def test_successful_policies_request(error_catcher, factory, policies_list):
+	path = reverse('policies')
+	request = factory.get(path)
+	response = policies.policies(request)
+	assert is_response_successful_with_pagination(response, len(policies_list))
 
 
 @catch_exception
-def test_unsuccessfully_policies_request(error_catcher, factory, db):
-    path = reverse('policies')
-    request = factory.get(path)
-    response = policies.policies(request)
-    assert is_response_empty(response)
+def test_successful_policies_request_with_pagination(error_catcher, factory, policies_list):
+	path = reverse('policies', kwargs={'entries_per_page': 10, 'page': 1})
+	request = factory.get(path)
+	response = policies.policies(request, entries_per_page=10, page=1)
+	assert is_response_successful_with_pagination(response, 10)
+
+
+@catch_exception
+def test_empty_policies_request(error_catcher, factory, db):
+	path = reverse('policies')
+	request = factory.get(path)
+	response = policies.policies(request)
+	assert is_response_empty_with_pagination(response)
+
+
+@catch_exception
+def test_unsuccessfully_policies_request(error_catcher, factory, policies_list):
+	path = reverse('policies', kwargs={'entries_per_page': 0, 'page': 1})
+	request = factory.get(path)
+	response = policies.policies(request, entries_per_page=0, page=1)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_successful_policy_request(error_catcher, factory, policy):
-    path = reverse('policy', kwargs={'policy_id': 1})
-    request = factory.get(path)
-    response = policies.policy(request, policy_id=1)
-    assert is_response_successful(response)
+	path = reverse('policy', kwargs={'policy_id': 1})
+	request = factory.get(path)
+	response = policies.policy(request, policy_id=1)
+	assert is_response_successful(response)
 
 
 @catch_exception
 def test_unsuccessfully_policy_request(error_catcher, factory, db):
-    path = reverse('policy', kwargs={'policy_id': 1})
-    request = factory.get(path)
-    response = policies.policy(request, policy_id=1)
-    assert is_response_unsuccessful(response)
+	path = reverse('policy', kwargs={'policy_id': 1})
+	request = factory.get(path)
+	response = policies.policy(request, policy_id=1)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_successful_bot_policies_request(error_catcher, factory, policy):
-    path = reverse('bot_policies', kwargs={'bot_id': 1})
-    request = factory.get(path)
-    response = policies.bot_policies(request, bot_id=1)
-    assert is_response_successful(response)
+	path = reverse('bot_policies', kwargs={'bot_id': 1})
+	request = factory.get(path)
+	response = policies.bot_policies(request, bot_id=1)
+	assert is_response_successful_with_pagination(response)
 
 
 @catch_exception
-def test_unsuccessfully_bot_policies_request(error_catcher, factory, db):
-    path = reverse('bot_policies', kwargs={'bot_id': 1})
-    request = factory.get(path)
-    response = policies.bot_policies(request, bot_id=1)
-    assert is_response_empty(response)
+def test_successful_bot_policies_request_with_pagination(error_catcher, factory, policy):
+	path = reverse('bot_policies', kwargs={'bot_id': 1, 'entries_per_page': 1, 'page': 1})
+	request = factory.get(path)
+	response = policies.bot_policies(request, bot_id=1, entries_per_page=1, page=1)
+	assert is_response_successful_with_pagination(response, 1)
+
+
+@catch_exception
+def test_empty_bot_policies_request(error_catcher, factory, db):
+	path = reverse('bot_policies', kwargs={'bot_id': 1})
+	request = factory.get(path)
+	response = policies.bot_policies(request, bot_id=1)
+	assert is_response_empty_with_pagination(response)
+
+
+@catch_exception
+def test_unsuccessfully_bot_policies_request(error_catcher, factory, policy):
+	path = reverse('bot_policies', kwargs={'bot_id': 1, 'entries_per_page': 0, 'page': 1})
+	request = factory.get(path)
+	response = policies.bot_policies(request, bot_id=1, entries_per_page=0, page=1)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_successful_add_policy_request(error_catcher, factory, policy):
-    assert add_bot_neo4j()
-    path = reverse('add_policy')
-    data = Policy_serializer(policy).data
-    data.pop('id', None)
-    data['tags'] = ["CDU"]
-    data['name'] = 'politica2'
-    request = factory.post(path, data, content_type='application/json')
-    response = policies.add_policy(request)
-    assert is_response_successful(response) and Policy.objects.filter().count() == 2
+	assert add_bot_neo4j()
+	path = reverse('add_policy')
+	data = Policy_serializer(policy).data
+	data.pop('id', None)
+	data['tags'] = ["CDU"]
+	data['name'] = 'politica2'
+	request = factory.post(path, data, content_type='application/json')
+	response = policies.add_policy(request)
+	assert is_response_successful(response) and Policy.objects.filter().count() == 2
 
 
 @catch_exception
 def test_unsuccessfully_add_policy_with_duplicated_params_policies_request(error_catcher, factory, policy):
-    assert add_bot_neo4j()
-    path = reverse('add_policy')
-    data = Policy_serializer(policy).data
-    data.pop('id', None)
-    request = factory.post(path, data, content_type='application/json')
-    response = policies.add_policy(request)
-    assert is_response_unsuccessful(response)
+	assert add_bot_neo4j()
+	path = reverse('add_policy')
+	data = Policy_serializer(policy).data
+	data.pop('id', None)
+	request = factory.post(path, data, content_type='application/json')
+	response = policies.add_policy(request)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_unsuccessfully_add_policy_with_invalid_bot_request(error_catcher, factory, policy):
-    assert add_bot_neo4j(2)
-    path = reverse('add_policy')
-    data = Policy_serializer(policy).data
-    data.pop('id', None)
-    request = factory.post(path, data, content_type='application/json')
-    response = policies.add_policy(request)
-    assert is_response_unsuccessful(response)
+	assert add_bot_neo4j(2)
+	path = reverse('add_policy')
+	data = Policy_serializer(policy).data
+	data.pop('id', None)
+	request = factory.post(path, data, content_type='application/json')
+	response = policies.add_policy(request)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_unsuccessfully_add_policy_request(error_catcher, factory, db):
-    path = reverse('add_policy')
-    request = factory.post(path, content_type='application/json')
-    response = policies.add_policy(request)
-    assert is_response_unsuccessful(response)
+	path = reverse('add_policy')
+	request = factory.post(path, content_type='application/json')
+	response = policies.add_policy(request)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_successful_remove_policy_request(error_catcher, factory, policy):
-    path = reverse('remove_policy', kwargs={'policy_id': 1})
-    request = factory.delete(path)
-    response = policies.remove_policy(request, policy_id=1)
-    assert is_response_successful(response) and Policy.objects.filter().count() == 0
+	path = reverse('remove_policy', kwargs={'policy_id': 1})
+	request = factory.delete(path)
+	response = policies.remove_policy(request, policy_id=1)
+	assert is_response_successful(response) and Policy.objects.filter().count() == 0
 
 
 @catch_exception
 def test_unsuccessfully_remove_policy_request(error_catcher, factory, db):
-    path = reverse('remove_policy', kwargs={'policy_id': 1})
-    request = factory.delete(path)
-    response = policies.remove_policy(request, policy_id=1)
-    assert is_response_unsuccessful(response)
+	path = reverse('remove_policy', kwargs={'policy_id': 1})
+	request = factory.delete(path)
+	response = policies.remove_policy(request, policy_id=1)
+	assert is_response_unsuccessful(response)
 
 
 @catch_exception
 def test_successful_update_policy_request(error_catcher, factory, policy):
-    path = reverse('update_policy', kwargs={'policy_id': 1})
-    request = factory.put(path, {'name': 'bot_1'}, content_type='application/json')
-    response = policies.update_policy(request, policy_id=1)
-    assert is_response_successful(response) and Policy.objects.filter().count() == 1
+	path = reverse('update_policy', kwargs={'policy_id': 1})
+	request = factory.put(path, {'name': 'bot_1'}, content_type='application/json')
+	response = policies.update_policy(request, policy_id=1)
+	assert is_response_successful(response) and Policy.objects.filter().count() == 1
 
 
 @catch_exception
 def test_unsuccessfully_update_policy_request(error_catcher, factory, db):
-    path = reverse('update_policy', kwargs={'policy_id': 1})
-    request = factory.put(path)
-    response = policies.update_policy(request, policy_id=1)
-    assert is_response_unsuccessful(response)
-
+	path = reverse('update_policy', kwargs={'policy_id': 1})
+	request = factory.put(path)
+	response = policies.update_policy(request, policy_id=1)
+	assert is_response_unsuccessful(response)
--- code/backend/twitter/run_tests.sh
+++ code/backend/twitter/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running Mongo tests..."
-python -m pytest api/tests/mongo_tests/* -x -o DJANGO_SETTINGS_MODULE=rest.mongodb_test_settings -
+python -m pytest api/tests/mongo_tests/* -x -o DJANGO_SETTINGS_MODULE=rest.mongodb_test_settings
 echo "Running Postgres tests..."
 python -m pytest api/tests/postgresql_tests/* -x -o DJANGO_SETTINGS_MODULE=rest.postgres_test_settings
 echo "Running Neo4j tests..."
