3890acbade07bca9ac3f8dce3e9337c55ce77db9
DrPunpun <pedrooliveira99@ua.pt>
2020-05-04 19:59:01 +0100

    code sucks with big F, but might work

--- code/backend/twitter/report_gen.py
+++ code/backend/twitter/report_gen.py
@@ -81,11 +81,21 @@ class Report:
 			return result
 		return None
 
-	def __insert_info_dict(self, info_dict, results_list, placement_dict):
+	def __insert_info_list(self, info_dict, results_list, placement_dict):
 		if results_list:
 			for result in results_list:
-				info_dict[placement_dict[result["id_str"]]] = result
+				index, key = placement_dict[result["id_str"]]
+				info_dict[index][key] = result
 		return info_dict
+
+	def __query_builder(self, query_tweets, query_bots, query_users, node):
+		node_label = node["labels"][0]
+		if node_label == "Tweet":
+			query_tweets.append(node["properties"]["id"])
+		elif node_label == "User":
+			query_users.append(node["properties"]["id"])
+		elif node_label == "Bot":
+			query_bots.append(node["properties"]["id"])
 
 	def create_report(self, match: dict, params: dict, limit: int = None, export='csv'):
 		query = f"MATCH r={self.__node_builder(match['start'])}" \
@@ -105,51 +115,72 @@ class Report:
 
 		start = time()
 
-		for row in query_result:
+		query_tweets_start = []
+		query_tweets_interm = []
+		query_tweets_end = []
+
+		query_bots_start = []
+		query_bots_interm = []
+		query_bots_end = []
+
+		query_user_start = []
+		query_user_interm = []
+		query_user_end = []
+
+		keep_track_places = {}
+
+		for row_index in range(len(query_result)):
 			# Analyse each row by looking at its rels field
+			row = query_result[row_index]
 			relations = row['r']['rels']
 			relation = {}
 
 			# Add the detailed start node
-			node_mongo_info = self.__get_mongo_info(relations[0]["start"], params['start'])
-			if node_mongo_info:
-				relation['start'] = node_mongo_info
-
-			query_tweets = []
-			query_users = []
-			query_bots = []
-
-			keep_track_places = {}
+			node_start = relations[0]["start"]
+			self.__query_builder(query_tweets_start, query_bots_start, query_user_start, node_start)
+			keep_track_places[node_start["properties"]["id"]] = (row_index, "start")
+			relation["start"] = {param: None for param in params['start'][node_start["labels"][0]]}
 
 			for index in range(len(relations) - 1):
 				rel = relations[index]
 				relation['rel' + str(index+1)] = {"name": rel["label"]}
-				node_label = rel["end"]["labels"][0]
-				if node_label == "Tweet":
-					query_tweets.append(rel["end"]["properties"]["id"])
-				elif node_label == "User":
-					query_users.append(rel["end"]["properties"]["id"])
-				elif node_label == "Bot":
-					query_bots.append(rel["end"]["properties"]["id"])
-				keep_track_places[rel["end"]["properties"]["id"]] = "interm" + str(index+1)
-				relation["interm" + str(index+1)] = {param:None for param in params['inter'][node_label]}
-
-			result_tweets = self.__get_mongo_aggregate("tweets", query_tweets, params['inter']['Tweet'])
-			result_users = self.__get_mongo_aggregate("users", query_users, params['inter']['User'])
-			result_bots = self.__get_mongo_aggregate("users", query_bots, params['inter']['Bot'])
-
-			relation = self.__insert_info_dict(relation, result_tweets, keep_track_places)
-			relation = self.__insert_info_dict(relation, result_users, keep_track_places)
-			relation = self.__insert_info_dict(relation, result_bots, keep_track_places)
+				self.__query_builder(query_tweets_interm, query_bots_interm, query_user_interm, rel["end"])
+				keep_track_places[rel["end"]["properties"]["id"]] = (row_index, "interm" + str(index + 1))
+				relation["interm" + str(index+1)] = {param: None for param in params['inter'][rel["end"]["labels"][0]]}
 
 			# Add ending node
 			relation['rel' + str(len(relations))] = {"name": relations[-1]["label"]}
-			node_mongo_info = self.__get_mongo_info(relations[-1]["end"], params['end'])
-			if node_mongo_info:
-				relation['end'] = node_mongo_info
+			node_end = relations[-1]["end"]
+			self.__query_builder(query_tweets_end, query_bots_end, query_user_end, node_end)
+			keep_track_places[node_end["properties"]["id"]] = (row_index, "end")
+			relation["end"] = {param: None for param in params['end'][node_end["labels"][0]]}
 
 			# Append to result
 			result.append(relation)
+
+		result_tweets_start = self.__get_mongo_aggregate("tweets", query_tweets_start, params['start']['Tweet'])
+		result_users_start = self.__get_mongo_aggregate("users", query_user_start, params['start']['User'])
+		result_bots_start = self.__get_mongo_aggregate("users", query_bots_start, params['start']['Bot'])
+
+		result_tweets_interm = self.__get_mongo_aggregate("tweets", query_tweets_interm, params['inter']['Tweet'])
+		result_users_interm = self.__get_mongo_aggregate("users", query_user_interm, params['inter']['User'])
+		result_bots_interm = self.__get_mongo_aggregate("users", query_bots_interm, params['inter']['Bot'])
+
+		result_tweets_end = self.__get_mongo_aggregate("tweets", query_tweets_end, params['end']['Tweet'])
+		result_users_end = self.__get_mongo_aggregate("users", query_user_end, params['end']['User'])
+		result_bots_end = self.__get_mongo_aggregate("users", query_bots_end, params['end']['Bot'])
+
+		result = self.__insert_info_list(result, result_tweets_start, keep_track_places)
+		result = self.__insert_info_list(result, result_users_start, keep_track_places)
+		result = self.__insert_info_list(result, result_bots_start, keep_track_places)
+
+		result = self.__insert_info_list(result, result_tweets_interm, keep_track_places)
+		result = self.__insert_info_list(result, result_users_interm, keep_track_places)
+		result = self.__insert_info_list(result, result_bots_interm, keep_track_places)
+
+		result = self.__insert_info_list(result, result_tweets_end, keep_track_places)
+		result = self.__insert_info_list(result, result_users_end, keep_track_places)
+		result = self.__insert_info_list(result, result_bots_end, keep_track_places)
 
 		logger.debug("It took " + str(time()-start))
 
@@ -212,7 +243,7 @@ if __name__ == '__main__':
 			'Bot': ['friends_count']
 		}
 	}
-	limit = 10000
+	limit = 1
 	for export_type in Report.ExportType:
 		print(export_type)
 		rep.create_report(query, params, limit, export_type)
