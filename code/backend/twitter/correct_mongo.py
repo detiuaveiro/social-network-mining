89492b2ad1887218891df4f7bb2ba9d0463a7e54
DrPunpun <pedrooliveira99@ua.pt>
2020-05-06 00:32:05 +0100

    corrected mongo

--- code/backend/twitter/correct_mongo.py
+++ code/backend/twitter/correct_mongo.py
@@ -1,5 +1,8 @@
 from wrappers.mongo_wrapper import MongoAPI
+from control_center import mongo_utils as utils
 import argparse
+import random
+import string
 
 mongo = MongoAPI()
 
@@ -11,7 +14,20 @@ def update_mongo(check_prints):
 		print(list_of_tweets)
 
 	for tweet in list_of_tweets:
-		mongo.update_tweets({"id_str": tweet["id_str"]}, {"user": mongo.users.find_one({"id": tweet["user"]})})
+		user_doc = mongo.users.find_one({"id": tweet["user"]})
+		if user_doc:
+			mongo.update_tweets({"id_str": tweet["id_str"]}, {"user": user_doc})
+		else:
+			if check_prints:
+				print("Found user none")
+			blank_user = utils.BLANK_USER.copy()
+			blank_user["id"] = tweet["user"]
+			blank_user["id_str"] = str(tweet["user"])
+
+			letters = string.ascii_lowercase
+			blank_user["screen_name"] = ''.join(random.choice(letters) for i in range(8))
+			mongo.insert_users(blank_user)
+			mongo.update_tweets({"id_str": tweet["id_str"]}, {"user": blank_user})
 
 	list_of_tweets = mongo.search("tweets", {"user": {"$type": "long"}}, ["id_str", "user"])
 	if check_prints:
