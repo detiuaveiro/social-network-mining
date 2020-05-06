fce4d6d0aa711fc0e6d2e07ddb178357bf104b6e
DrPunpun <pedrooliveira99@ua.pt>
2020-05-06 17:56:32 +0100

    corrected

--- code/backend/twitter/correct_mongo.py
+++ code/backend/twitter/correct_mongo.py
@@ -15,19 +15,7 @@ def update_mongo(check_prints):
 
 	for tweet in list_of_tweets:
 		user_doc = mongo.users.find_one({"id": tweet["user"]})
-		if user_doc:
-			mongo.update_tweets({"id_str": tweet["id_str"]}, {"user": user_doc})
-		else:
-			if check_prints:
-				print("Found user none")
-			blank_user = utils.BLANK_USER.copy()
-			blank_user["id"] = tweet["user"]
-			blank_user["id_str"] = str(tweet["user"])
-
-			letters = string.ascii_lowercase
-			blank_user["screen_name"] = ''.join(random.choice(letters) for i in range(8))
-			mongo.insert_users(blank_user)
-			mongo.update_tweets({"id_str": tweet["id_str"]}, {"user": blank_user})
+		mongo.update_tweets({"id_str": tweet["id_str"]}, {"user": user_doc})
 
 	list_of_tweets = mongo.search("tweets", {"user": {"$type": "long"}}, ["id_str", "user"])
 	if check_prints:
