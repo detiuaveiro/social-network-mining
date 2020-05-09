a6d66266096205ad07a7f4f815cca0c2ed23ce14
escaleira <pedroescaleira@hotmail.com>
2020-05-09 16:01:25 +0100

    trying to avoid the timeout

--- code/backend/twitter/wrappers/rabbitmq_wrapper.py
+++ code/backend/twitter/wrappers/rabbitmq_wrapper.py
@@ -51,8 +51,8 @@ class Rabbitmq:
             port=port,
             virtual_host=vhost,
             credentials=pika_credentials,
-            heartbeat=0,
-            blocked_connection_timeout=0
+            heartbeat=30000,
+            blocked_connection_timeout=30000
         )
 
         self._connection = None
