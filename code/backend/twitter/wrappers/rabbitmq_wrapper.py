544baf9547cf8903a74e1172a0d6c47383b98ec9
escaleira <pedroescaleira@hotmail.com>
2020-05-14 16:43:42 +0100

    problems with pika

--- code/backend/twitter/wrappers/rabbitmq_wrapper.py
+++ code/backend/twitter/wrappers/rabbitmq_wrapper.py
@@ -168,8 +168,7 @@ class Rabbitmq:
         log.info("Connection to Rabbit Established")
 
     def __set_prefetch(self, _unused_frame, queue):
-        self.channels[queue].basic_qos(prefetch_count=30, callback=functools.partial(self._receive, queue=queue),
-                                       global_qos=True)
+        self.channels[queue].basic_qos(prefetch_count=30, callback=functools.partial(self._receive, queue=queue))
 
     def __on_connection_open_error(self, _unused_connection, err):
         """This method is called by pika if the connection to RabbitMQ
@@ -204,7 +203,7 @@ class Rabbitmq:
         log.info('Stopping')
         self.__stop_consuming()
         log.info('Stopped')
-        time.sleep(2)
+        # time.sleep(2)
         self.__connect()
 
     def __stop_consuming(self):
