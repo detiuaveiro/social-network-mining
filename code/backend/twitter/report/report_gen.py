3e0cb076952390fe3210a95f4850ff9f36e016fe
Rafaelyot <rafaeljsimoes@gmail.com>
2020-05-24 19:50:03 +0100

    Minor fix

--- code/backend/twitter/report/report_gen.py
+++ code/backend/twitter/report/report_gen.py
@@ -70,7 +70,15 @@ class Exporter:
 
 class Report:
 	mongo = MongoAPI()
-	neo = Neo4jAPI()
+	NEO4J_PORT = os.environ.get('NEO4J_PORT', None)
+
+	if NEO4J_PORT:
+		FULL_URL = f'neo4j:{NEO4J_PORT}'
+		print(FULL_URL)
+		neo4j = Neo4jAPI(FULL_URL)
+	else:
+		neo4j = Neo4jAPI()
+
 	exporter = Exporter(EXPORT_DIR)
 
 	@staticmethod
