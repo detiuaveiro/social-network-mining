d60ba8418709c9b06e3fed184213c08c9c0e5ac3
DrPunpun <pedrooliveira99@ua.pt>
2020-05-24 20:07:05 +0100

    just need to test wiht bot

--- code/backend/twitter/control_center/tests/test_redis.py
+++ code/backend/twitter/control_center/tests/test_redis.py
@@ -1,2 +1,24 @@
 import redis
+import json
+import time
 
+r = redis.Redis()
+
+
+def to_json(data: dict) -> str:
+	return json.dumps(data, separators=(',', ':'))
+
+
+def test_redis_ttl():
+	data = {
+		'type': 'message_type',
+		'data': ['data', 'lel']
+	}
+	r.set(to_json(data), "")
+	r.expire(to_json(data), 1)
+
+	time.sleep(1)
+	if r.get(to_json):
+		return "Something went wrong"
+
+	return "Everything's good"
--- code/backend/twitter/tests_cc.py
+++ code/backend/twitter/tests_cc.py
@@ -1,7 +1,8 @@
 from control_center.tests.save_tweet_relations import test
-from control_center.tests import test_logs, test_psql
+from control_center.tests import test_logs, test_psql, test_redis
 from wrappers.postgresql_wrapper import PostgresAPI
 from report.report_gen import Report
+
 
 def main():
 	#test_json()
@@ -9,7 +10,7 @@ def main():
 	#test_logs.test_logs_ts()
 	#test_logs.test_logs()
 	#test_psql.test_insert_user_with_protected()
-
+	print(test_redis.test_redis_ttl())
 
 if __name__ == "__main__":
 	main()
