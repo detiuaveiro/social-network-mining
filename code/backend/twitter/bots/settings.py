13eb7764edacd5568020e52ed6b3e1b72e97aa84
escaleira <pedroescaleira@hotmail.com>
2020-05-25 17:59:54 +0100

    reducing the im alive time

--- code/backend/twitter/bots/settings.py
+++ code/backend/twitter/bots/settings.py
@@ -29,5 +29,5 @@ REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 
 WAIT_TIME_BETWEEN_WORK = 60*60*2            # two hours
 WAIT_TIME_RANDOM_STOP = 60*30               # half an hour
-WAIT_TIME_IM_ALIVE = 60*5                   # five minutes
+WAIT_TIME_IM_ALIVE = 30                     # 30 seconds
 BOT_TTL = 60*60*24							# one day
