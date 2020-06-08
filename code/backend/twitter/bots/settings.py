a7a40c5b61cf548405679e6dbddce4c206eff5f1
escaleira <pedroescaleira@hotmail.com>
2020-06-09 00:38:30 +0100

    small change

--- code/backend/twitter/bots/settings.py
+++ code/backend/twitter/bots/settings.py
@@ -28,6 +28,6 @@ REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 # -----------------------------------------------------------
 
 WAIT_TIME_BETWEEN_WORK = 60*60*3            # three hours
-WAIT_TIME_RANDOM_STOP = 60*60               # an hour
+WAIT_TIME_RANDOM_STOP = 60*60*0.5           # half an hour
 WAIT_TIME_NO_MESSAGES = 60*15               # 15 minutes
 BOT_TTL = 60*60*24							# one day
